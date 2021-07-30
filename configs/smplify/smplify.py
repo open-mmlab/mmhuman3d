0edeb9e533fba512c4e3919618a23a986504449a
caizhongang <cai.zhongang@gmail.com>
2021-07-30 14:42:57 +0800

    Add parametric model toolbox

---
+++ configs/smplify/smplify.py
@@ -0,0 +1,15 @@
+smplify_stages = {
+    'Stage 1': {
+        'num_iter': 10,
+    },
+    'Stage 2': {
+        'num_iter': 1,
+    },
+}
+
+smplify_opt_config = {
+    'type': 'lbfgs',
+    'max_iter': 20,
+    'lr': 1e-2,
+    'line_search_fn': 'strong_wolfe'
+}
