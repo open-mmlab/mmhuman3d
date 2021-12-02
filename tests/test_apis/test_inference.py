20a7f8fc04ebd508e98edca5e5f3c76aa6c823c0
gaoyang <gaoyang_cn@outlook.com>
2021-12-02 15:06:36 +0800

    fix test api for cpu and use secret

--- tests/test_apis/test_inference.py
+++ tests/test_apis/test_inference.py
@@ -13,12 +13,11 @@ def test_inference_model():
     mesh_model = init_model(
         'configs/spin/resnet50_spin_pw3d.py', None, device=device_name)
 
-    # frames_iter = prepare_frames(
-    #     video_path='demo/resources/multi_person_demo.mp4')
-    frames_iter = np.ones([224, 224, 3])
-    person_results = [{'track_id': 0, 'bbox': [0, 0, 224, 224, 1]}]
-    mesh_results = inference_model(mesh_model, frames_iter, person_results)
-    pred_cams = mesh_results[0]['camera'][None]
-    verts = mesh_results[0]['vertices'][None]
-    bboxes_xy = mesh_results[0]['bbox'][None]
-    _, _ = conver_verts_to_cam_coord(verts, pred_cams, bboxes_xy)
+    if torch.cuda.is_available():
+        frames_iter = np.ones([224, 224, 3])
+        person_results = [{'track_id': 0, 'bbox': [0, 0, 224, 224, 1]}]
+        mesh_results = inference_model(mesh_model, frames_iter, person_results)
+        pred_cams = mesh_results[0]['camera'][None]
+        verts = mesh_results[0]['vertices'][None]
+        bboxes_xy = mesh_results[0]['bbox'][None]
+        _, _ = conver_verts_to_cam_coord(verts, pred_cams, bboxes_xy)
