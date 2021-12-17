a66ea9b3e06b3c48fa18262f17accc29e616a6e6
sunqingping <sunqingping@sensetime.com>
2021-12-17 19:52:15 +0800

    fix unit test

--- tests/test_apis/test_inference.py
+++ tests/test_apis/test_inference.py
@@ -1,6 +1,5 @@
 import mmcv
 import numpy as np
-import torch
 
 from mmhuman3d.apis import (
     feature_extract,
@@ -11,6 +10,8 @@ from mmhuman3d.apis import (
 from mmhuman3d.utils.demo_utils import (
     conver_verts_to_cam_coord,
     extract_feature_sequence,
+    process_mmdet_results,
+    smooth_process,
 )
 
 
@@ -20,15 +21,15 @@ def test_inference_image_based_model():
     config.model.backbone.norm_cfg = dict(type='BN', requires_grad=True)
     mesh_model, _ = init_model(config, None, device=device_name)
 
-    if torch.cuda.is_available():
-        frames_iter = np.ones([224, 224, 3])
-        person_results = [{'track_id': 0, 'bbox': [0, 0, 224, 224, 1]}]
-        mesh_results = inference_image_based_model(mesh_model, frames_iter,
-                                                   person_results)
-        pred_cams = mesh_results[0]['camera'][None]
-        verts = mesh_results[0]['vertices'][None]
-        bboxes_xy = mesh_results[0]['bbox'][None]
-        _, _ = conver_verts_to_cam_coord(verts, pred_cams, bboxes_xy)
+    frames_iter = np.ones([224, 224, 3])
+    person_results = [{'track_id': 0, 'bbox': [0, 0, 224, 224, 1]}]
+    mesh_results = inference_image_based_model(mesh_model, frames_iter,
+                                               person_results)
+    pred_cams = mesh_results[0]['camera'][None]
+    verts = mesh_results[0]['vertices'][None]
+    bboxes_xy = mesh_results[0]['bbox'][None]
+    smooth_process(verts.repeat(20, 0))
+    _, _ = conver_verts_to_cam_coord(verts, pred_cams, bboxes_xy)
 
 
 def test_inference_video_based_model():
@@ -37,24 +38,25 @@ def test_inference_video_based_model():
     config.extractor.checkpoint = None
     config.extractor.backbone.norm_cfg = dict(type='BN', requires_grad=True)
     mesh_model, extractor = init_model(config, None, device=device_name)
-    if torch.cuda.is_available():
-        frames_iter = np.ones([224, 224, 3])
-        person_results = [{'track_id': 0, 'bbox': [0, 0, 224, 224, 1]}]
-        person_results = feature_extract(extractor, frames_iter,
-                                         person_results)
-        person_results_list = [person_results]
-        feature_results_seq = extract_feature_sequence(
-            person_results_list, frame_idx=0, causal=True, seq_len=16, step=1)
-        inference_video_based_model(
-            mesh_model,
-            extracted_results=feature_results_seq,
-            with_track_id=True)
-        inference_video_based_model(
-            mesh_model,
-            extracted_results=feature_results_seq,
-            with_track_id=False)
+
+    frames_iter = np.ones([224, 224, 3])
+    person_results = [{'track_id': 0, 'bbox': [0, 0, 224, 224, 1]}]
+    person_results = feature_extract(extractor, frames_iter, person_results)
+    person_results_list = [person_results]
+    feature_results_seq = extract_feature_sequence(
+        person_results_list, frame_idx=0, causal=True, seq_len=16, step=1)
+    inference_video_based_model(
+        mesh_model, extracted_results=feature_results_seq, with_track_id=True)
+
+    inference_video_based_model(
+        mesh_model, extracted_results=feature_results_seq, with_track_id=False)
 
 
-if __name__ == '__main__':
-    test_inference_image_based_model()
-    test_inference_video_based_model()
+def test_process_mmdet_results():
+    det_results = [np.array([0, 0, 100, 100])]
+    det_mask_results = None
+
+    _ = process_mmdet_results(
+        mmdet_results=(det_results, det_mask_results), cat_id=1)
+
+    _ = process_mmdet_results(mmdet_results=det_results, cat_id=1)
