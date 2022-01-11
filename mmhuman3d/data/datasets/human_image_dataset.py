2d63daad0306e15fd329a5f5c5583980d4cc9fad
caizhongang <cai.zhongang@gmail.com>
2022-01-11 21:09:17 +0800

    bug fix

--- mmhuman3d/data/datasets/human_image_dataset.py
+++ mmhuman3d/data/datasets/human_image_dataset.py
@@ -109,7 +109,7 @@ class HumanImageDataset(BaseDataset, metaclass=ABCMeta):
         image_path = self.human_data['image_path'][idx]
         info['image_path'] = os.path.join(self.data_prefix, 'datasets',
                                           self.dataset_name, image_path)
-        if 'image_path'.endswith('smc'):
+        if image_path.endswith('smc'):
             info['image_id'] = self.human_data['image_id'][idx]
         info['dataset_name'] = self.dataset_name
         info['sample_idx'] = idx
