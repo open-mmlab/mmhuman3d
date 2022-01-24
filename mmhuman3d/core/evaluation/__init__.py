fce421252e3258484ef9f147c62ed81ae37732f3
sunqingping <sunqingping@sensetime.com>
2022-01-24 15:27:11 +0800

    fix lint

--- mmhuman3d/core/evaluation/__init__.py
+++ mmhuman3d/core/evaluation/__init__.py
@@ -1,8 +1,9 @@
 from mmhuman3d.core.evaluation import mesh_eval, mpjpe
+from mmhuman3d.core.evaluation.eval_hooks import DistEvalHook, EvalHook
 from mmhuman3d.core.evaluation.mesh_eval import compute_similarity_transform
 from mmhuman3d.core.evaluation.mpjpe import keypoint_mpjpe
-from mmhuman3d.core.evaluation.eval_hooks import DistEvalHook,EvalHook
 
 __all__ = [
-    'compute_similarity_transform', 'keypoint_mpjpe', 'mesh_eval', 'mpjpe', 'DistEvalHook', 'EvalHook'
+    'compute_similarity_transform', 'keypoint_mpjpe', 'mesh_eval', 'mpjpe',
+    'DistEvalHook', 'EvalHook'
 ]
