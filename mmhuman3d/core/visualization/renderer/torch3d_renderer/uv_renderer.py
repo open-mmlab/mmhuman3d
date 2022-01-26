e892f252fbf50182784199fa72d5248d2f955010
wangwenjia <wangwenjia@sensetime.com>
2022-01-27 01:10:21 +0800

    store

--- mmhuman3d/core/visualization/renderer/torch3d_renderer/uv_renderer.py
+++ mmhuman3d/core/visualization/renderer/torch3d_renderer/uv_renderer.py
@@ -1,6 +1,7 @@
+from collections import namedtuple
 import pickle
 import warnings
-from typing import Iterable, Optional, Union
+from typing import Iterable, Optional, Tuple, Union
 
 import numpy as np
 import torch
@@ -11,8 +12,10 @@ from pytorch3d.ops import interpolate_face_attributes
 from pytorch3d.renderer.mesh import TexturesUV
 from pytorch3d.structures import Meshes
 from pytorch3d.structures.utils import padded_to_packed
-
-from mmhuman3d.core.cameras.cameras import NewAttributeCameras
+from pytorch3d.renderer.mesh.rasterizer import (MeshRasterizer,
+                                                RasterizationSettings)
+from mmhuman3d.core.cameras.cameras import (NewAttributeCameras,
+                                            FoVOrthographicCameras)
 from mmhuman3d.models.body_models import SMPL, SMPLX
 from mmhuman3d.utils.path_utils import check_path_suffix
 
@@ -53,29 +56,26 @@ class UVRenderer(nn.Module):
             check_path_suffix(param_path, allowed_suffix=['pkl', 'pickle'])
             with open(param_path, 'rb') as f:
                 param_dict = pickle.load(f)
-            self.bary_coords = torch.Tensor(param_dict['bary_weights'])
-            verts_uv = torch.Tensor(param_dict['texcoords'])
+            self.bary_coords = torch.Tensor(param_dict['bary_weights']).to(
+                self.device)
+            verts_uv = torch.Tensor(param_dict['texcoords']).to(self.device)
             verts_u, verts_v = torch.unbind(verts_uv, -1)
             verts_v_ = 1 - verts_u.unsqueeze(-1)
             verts_u_ = verts_v.unsqueeze(-1)
-            self.verts_uv = torch.cat([verts_u_, verts_v_], -1)
-            self.faces_uv = torch.LongTensor(param_dict['vt_faces'])
+            self.verts_uv = torch.cat([verts_u_, verts_v_], -1).to(self.device)
+            self.faces_uv = torch.LongTensor(param_dict['vt_faces']).to(
+                self.device)
             self.vt_to_v = param_dict['vt_to_v']
             self.v_to_vt = param_dict['vt_to_v']
             self.NUM_VT = self.verts_uv.shape[0]
-            self.pix_to_face = torch.LongTensor(param_dict['face_id'])
+            self.pix_to_face = torch.LongTensor(param_dict['face_id']).to(
+                self.device)
             self.face_tensor = torch.LongTensor(param_dict['faces'].astype(
-                np.int64))
+                np.int64)).to(self.device)
             self.num_faces = self.faces_uv.shape[0]
-            if resolution != (256, 256):
-                self.bary_coords = F.interpolate(
-                    self.bary_coords[None], resolution, mode='bicubic').clamp(
-                        min=0, max=1)[0]
-                self.pix_to_face = F.interpolate(
-                    self.pix_to_face[None], resolution,
-                    mode='bicubic').long()[0]
+
             self.vt_to_v_index = torch.LongTensor(
-                [self.vt_to_v[i] for i in range(self.NUM_VT)])
+                [self.vt_to_v[i] for i in range(self.NUM_VT)]).to(self.device)
         elif obj_path is not None:
             check_path_suffix(obj_path, allowed_suffix=['obj'])
             mesh_template = load_objs_as_meshes([obj_path])
@@ -83,7 +83,33 @@ class UVRenderer(nn.Module):
             self.verts_uv = mesh_template.textures.verts_uvs_padded()[0]
             self.num_faces = self.faces_uv.shape[0]
 
-    def forward(self, verts_attr: Optional[torch.Tensor]) -> torch.Tensor:
+    def raster_fragments(
+            self,
+            resolution: Optional[Iterable[int]] = None) -> Tuple[torch.Tensor]:
+        rasterizer = MeshRasterizer(
+            cameras=FoVOrthographicCameras(
+                min_x=1, max_x=0, max_y=1, min_y=0, device=self.device),
+            raster_settings=RasterizationSettings(
+                blur_radius=0,
+                image_size=resolution,
+                faces_per_pixel=1,
+                perspective_correct=False,
+            )).to(self.device)
+        verts_uv = torch.cat([
+            self.verts_uv[None],
+            torch.ones(1, self.NUM_VT, 1).to(self.device)
+        ], -1)
+
+        fragments = rasterizer(
+            Meshes(verts=verts_uv, faces=self.faces_uv[None]))
+        pix_to_face = fragments.pix_to_face[0, ..., 0]
+        bary_coords = fragments.bary_coords[0, ..., 0, :]
+        mask = (pix_to_face >= 0).long()
+        return pix_to_face, bary_coords, mask
+
+    def forward(self,
+                verts_attr: Optional[torch.Tensor],
+                resolution: Optional[Iterable[int]] = None) -> torch.Tensor:
         """[summary]
 
         Args:
@@ -94,12 +120,18 @@ class UVRenderer(nn.Module):
         """
         if verts_attr.ndim == 2:
             verts_attr = verts_attr[None]
+        if resolution is not None and resolution != self.resolution:
+            pix_to_face, bary_coords, _ = self.raster_fragments(
+                resolution=resolution)
+        else:
+            bary_coords = self.bary_coords
+            pix_to_face = self.pix_to_face
         N, V, D = verts_attr.shape
         assert V == self.NUM_VERTS
-        verts_attr = verts_attr.view(N * V, D)
+        verts_attr = verts_attr.view(N * V, D).to(self.device)
         offset_idx = torch.arange(0, N).long() * (self.NUM_VERTS - 1)
         faces_packed = self.face_tensor[None].repeat(
-            N, 1, 1) + offset_idx.view(-1, 1, 1)
+            N, 1, 1) + offset_idx.view(-1, 1, 1).to(self.device)
         faces_packed = faces_packed.view(-1, 3)
         face_attr = verts_attr[faces_packed]
         assert face_attr.shape == (N * self.num_faces, 3, D)
@@ -118,24 +150,23 @@ class UVRenderer(nn.Module):
             meshes: Meshes,
             resolution: Optional[Iterable[int]] = None,
             cameras: NewAttributeCameras = None) -> torch.Tensor:
-        resolution = resolution if resolution is not None else self.resolution
         verts_normals = meshes.verts_normals_padded()
         if cameras:
             verts_normals = cameras.get_world_to_view_transform(
             ).transform_normals(verts_normals)
-        normal_map = self.forward(verts_attr=verts_normals)
+        normal_map = self.forward(
+            verts_attr=verts_normals, resolution=resolution)
         return normal_map
 
     def forward_uvd_map(self,
                         meshes: Meshes,
                         resolution: Optional[Iterable[int]] = None,
                         cameras: NewAttributeCameras = None) -> torch.Tensor:
-        resolution = resolution if resolution is not None else self.resolution
         verts_uvd = meshes.verts_padded()
         if cameras:
             verts_uvd = cameras.get_world_to_view_transform(
             ).transform_normals(verts_uvd)
-        uvd_map = self.forward(verts_attr=verts_uvd)
+        uvd_map = self.forward(verts_attr=verts_uvd, resolution=resolution)
         return uvd_map
 
     def resample(self,
@@ -164,19 +195,17 @@ class UVRenderer(nn.Module):
 
         offset_idx = torch.arange(0, N).long() * (self.NUM_VERTS - 1)
         faces_packed = self.face_tensor[None].repeat(
-            N, 1, 1) + offset_idx.view(-1, 1, 1)
+            N, 1, 1) + offset_idx.view(-1, 1, 1).to(self.device)
         faces_packed = faces_packed.view(-1, 3)
 
         verts_feature = torch.zeros(N * self.NUM_VERTS, 3).to(self.device)
 
         verts_feature[faces_packed.view(
             -1,
-            3)] = maps_padded[:, triangle_uv[:, 0],
+            3)] = maps_padded[:, H - triangle_uv[:, 0],
                               triangle_uv[:, 1]].view(N * self.num_faces, 3, C)
         verts_feature = verts_feature.view(N, self.NUM_VERTS, 3)
 
-        from IPython import embed
-        embed()
         return verts_feature
 
     def warp_normal_map(
