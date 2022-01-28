fc6afd4d6f77510903ed7364c6f49269da5a1166
ttxskk <sunqingpings@gmail.com>
2022-01-28 22:34:43 +0800

    create the folder if it does not exist

--- tools/test.py
+++ tools/test.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import os.path as osp
 
 import mmcv
 import torch
@@ -132,6 +133,8 @@ def main():
     eval_cfg = cfg.get('evaluation', args.metric_options)
     eval_cfg.update(dict(metric=args.metrics))
     if rank == 0:
+        if args.work_dir is not None:
+            mmcv.mkdir_or_exist(osp.abspath(args.work_dir))
         results = dataset.evaluate(outputs, args.work_dir, **eval_cfg)
         for k, v in results.items():
             print(f'\n{k} : {v:.2f}')
