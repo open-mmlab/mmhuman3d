910d66d5438a1449a6e71eaf2036f3e6a3d98e10
JasonBoy1 <wwj940312@126.com>
2022-02-06 11:20:20 +0800

    merge

--- tools/run_f2a.sh
+++
@@ -1,8 +0,0 @@
-srun -p Sensetime \
-    --job-name="python" \
-    --gres=gpu:1 \
-    --ntasks=1 \
-    --ntasks-per-node=1 \
-    --cpus-per-task=4 \
-    --kill-on-bad-exit=1 \
-    python -u run_f2a.py --exp_dir=$1
