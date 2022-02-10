5045dcd76663c2758c159f4c1990f4e3efcfc789
caizhongang <cai.zhongang@gmail.com>
2022-02-10 14:31:50 +0800

    Remove unused import

--- mmhuman3d/data/data_structures/smc_reader.py
+++ mmhuman3d/data/data_structures/smc_reader.py
@@ -5,10 +5,6 @@ import h5py
 import numpy as np
 import torch
 import tqdm
-from mmhuman3d.utils.transforms import (
-    aa_to_rotmat,
-    rotmat_to_aa
-)
 
 from mmhuman3d.models.body_models.utils import batch_transform_to_camera_frame
 from mmhuman3d.models.builder import build_body_model
