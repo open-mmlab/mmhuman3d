d8e5014f84a0529b2cb8cc375486ab93c0a95268
caizhongang <cai.zhongang@gmail.com>
2022-02-10 19:47:38 +0800

    Remove unsupported unit test and bug fix

--- tests/test_models/test_body_models/test_smpl.py
+++ tests/test_models/test_body_models/test_smpl.py
@@ -3,7 +3,7 @@ import torch
 from mmhuman3d.models.builder import build_body_model
 
 body_model_load_dir = 'data/body_models/smpl'
-extra_joints_regressor = 'data/J_regressor_extra.npy'
+extra_joints_regressor = 'data/body_models/J_regressor_extra.npy'
 
 
 def test_smpl():
@@ -43,57 +43,57 @@ def test_smpl():
     assert torch.allclose(smpl_54_joints[:, joint_mapping, :], smpl_49_joints)
 
 
-def test_gendered_smpl():
-    random_betas_neutral = torch.rand((1, 10))
-    random_betas_male = torch.rand((1, 10))
-    random_betas_female = torch.rand((1, 10))
-    gender = torch.Tensor([-1, 0, 1])
-
-    smpl_neutral = build_body_model(
-        dict(
-            type='SMPL',
-            gender='neutral',
-            keypoint_src='smpl_45',
-            keypoint_dst='smpl_45',
-            model_path=body_model_load_dir,
-        ))
-
-    smpl_male = build_body_model(
-        dict(
-            type='SMPL',
-            gender='male',
-            keypoint_src='smpl_45',
-            keypoint_dst='smpl_45',
-            model_path=body_model_load_dir,
-        ))
-
-    smpl_female = build_body_model(
-        dict(
-            type='SMPL',
-            gender='female',
-            keypoint_src='smpl_45',
-            keypoint_dst='smpl_45',
-            model_path=body_model_load_dir,
-        ))
-
-    gendered_smpl = build_body_model(
-        dict(
-            type='GenderedSMPL',
-            keypoint_src='smpl_45',
-            keypoint_dst='smpl_45',
-            model_path=body_model_load_dir))
-
-    smpl_neutral_output = smpl_neutral(betas=random_betas_neutral)
-    smpl_male_output = smpl_male(betas=random_betas_male)
-    smpl_female_output = smpl_female(betas=random_betas_female)
-
-    betas_concat = torch.cat(
-        [random_betas_neutral, random_betas_male, random_betas_female])
-    joint_concat = torch.cat([
-        smpl_neutral_output['joints'], smpl_male_output['joints'],
-        smpl_female_output['joints']
-    ])
-
-    gendered_smpl_output = gendered_smpl(betas=betas_concat, gender=gender)
-
-    assert torch.allclose(joint_concat, gendered_smpl_output['joints'])
+# def test_gendered_smpl():
+#     random_betas_neutral = torch.rand((1, 10))
+#     random_betas_male = torch.rand((1, 10))
+#     random_betas_female = torch.rand((1, 10))
+#     gender = torch.Tensor([-1, 0, 1])
+#
+#     smpl_neutral = build_body_model(
+#         dict(
+#             type='SMPL',
+#             gender='neutral',
+#             keypoint_src='smpl_45',
+#             keypoint_dst='smpl_45',
+#             model_path=body_model_load_dir,
+#         ))
+#
+#     smpl_male = build_body_model(
+#         dict(
+#             type='SMPL',
+#             gender='male',
+#             keypoint_src='smpl_45',
+#             keypoint_dst='smpl_45',
+#             model_path=body_model_load_dir,
+#         ))
+#
+#     smpl_female = build_body_model(
+#         dict(
+#             type='SMPL',
+#             gender='female',
+#             keypoint_src='smpl_45',
+#             keypoint_dst='smpl_45',
+#             model_path=body_model_load_dir,
+#         ))
+#
+#     gendered_smpl = build_body_model(
+#         dict(
+#             type='GenderedSMPL',
+#             keypoint_src='smpl_45',
+#             keypoint_dst='smpl_45',
+#             model_path=body_model_load_dir))
+#
+#     smpl_neutral_output = smpl_neutral(betas=random_betas_neutral)
+#     smpl_male_output = smpl_male(betas=random_betas_male)
+#     smpl_female_output = smpl_female(betas=random_betas_female)
+#
+#     betas_concat = torch.cat(
+#         [random_betas_neutral, random_betas_male, random_betas_female])
+#     joint_concat = torch.cat([
+#         smpl_neutral_output['joints'], smpl_male_output['joints'],
+#         smpl_female_output['joints']
+#     ])
+#
+#     gendered_smpl_output = gendered_smpl(betas=betas_concat, gender=gender)
+#
+#     assert torch.allclose(joint_concat, gendered_smpl_output['joints'])
