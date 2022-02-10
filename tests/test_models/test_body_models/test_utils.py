05bc62d06964dd873c4d94f6d95ec0a03bcfc503
caizhongang <cai.zhongang@gmail.com>
2022-02-10 16:58:34 +0800

    format

--- tests/test_models/test_body_models/test_utils.py
+++ tests/test_models/test_body_models/test_utils.py
@@ -1,6 +1,10 @@
 import torch
+
+from mmhuman3d.models.body_models import (
+    batch_transform_to_camera_frame,
+    transform_to_camera_frame,
+)
 from mmhuman3d.models.builder import build_body_model
-from mmhuman3d.models.body_models import transform_to_camera_frame, batch_transform_to_camera_frame
 from mmhuman3d.utils.transforms import ee_to_rotmat
 
 
@@ -13,8 +17,7 @@ def test_transform_to_camera_frame():
             keypoint_src='smpl_45',
             keypoint_dst='smpl_45',
             model_path='data/body_models/smpl',
-        )
-    )
+        ))
 
     # generate random values
     random_transl = torch.rand((1, 3))
@@ -34,8 +37,7 @@ def test_transform_to_camera_frame():
         global_orient=random_global_orient,
         body_pose=random_body_pose,
         transl=random_transl,
-        betas=random_betas
-    )
+        betas=random_betas)
 
     random_joints = random_output['joints']
     random_pelvis = random_joints[:, 0, :]
@@ -53,15 +55,16 @@ def test_transform_to_camera_frame():
         global_orient=torch.tensor(transformed_global_orient.reshape(1, 3)),
         transl=torch.tensor(transformed_transl.reshape(1, 3)),
         body_pose=random_body_pose,
-        betas=random_betas
-    )
+        betas=random_betas)
 
     transformed_joints = transformed_output['joints']
 
     # check validity
     random_joints = random_joints.squeeze()  # (45, 3)
-    random_joints = torch.cat([random_joints, torch.ones(45, 1)], dim=1)  # (45, 4)
-    test_joints = torch.einsum('ij,kj->ki', random_extrinsic, random_joints)  # (45, 4)
+    random_joints = torch.cat([random_joints, torch.ones(45, 1)],
+                              dim=1)  # (45, 4)
+    test_joints = torch.einsum('ij,kj->ki', random_extrinsic,
+                               random_joints)  # (45, 4)
     test_joints = test_joints[:, :3]  # (45, 3)
     assert torch.allclose(transformed_joints, test_joints)
 
@@ -77,8 +80,7 @@ def test_batch_transform_to_camera_frame():
             keypoint_src='smpl_45',
             keypoint_dst='smpl_45',
             model_path='data/body_models/smpl',
-        )
-    )
+        ))
 
     # generate random values
     random_transl = torch.rand((1, 3))
@@ -98,8 +100,7 @@ def test_batch_transform_to_camera_frame():
         global_orient=random_global_orient,
         body_pose=random_body_pose,
         transl=random_transl,
-        betas=random_betas
-    )
+        betas=random_betas)
 
     random_joints = random_output['joints']
     random_pelvis = random_joints[:, 0, :]
@@ -117,14 +118,15 @@ def test_batch_transform_to_camera_frame():
         global_orient=torch.tensor(transformed_global_orient.reshape(N, 3)),
         transl=torch.tensor(transformed_transl.reshape(N, 3)),
         body_pose=random_body_pose,
-        betas=random_betas
-    )
+        betas=random_betas)
 
     transformed_joints = transformed_output['joints']
 
     # check validity
     random_joints = random_joints  # (N, 45, 3)
-    random_joints = torch.cat([random_joints, torch.ones(N, 45, 1)], dim=2)  # (N, 45, 4)
-    test_joints = torch.einsum('ij,bkj->bki', random_extrinsic, random_joints)  # (N, 45, 4)
+    random_joints = torch.cat(
+        [random_joints, torch.ones(N, 45, 1)], dim=2)  # (N, 45, 4)
+    test_joints = torch.einsum('ij,bkj->bki', random_extrinsic,
+                               random_joints)  # (N, 45, 4)
     test_joints = test_joints[:, :, :3]  # (N, 45, 3)
     assert torch.allclose(transformed_joints, test_joints)
