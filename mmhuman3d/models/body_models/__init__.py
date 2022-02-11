6f8752f136a62cd8cccd9a56daa98427569654e8
Zhongang Cai <62529255+caizhongang@users.noreply.github.com>
2022-02-11 09:30:59 +0800

    [Feature] Upgrade SMCReader to support rigid transformation of SMPL (#100)
    * Fix SMPL transformation
    
    * Fix circular import
    
    * Add utils for (batch) transformation of smpl
    
    * Allow flexible body_model input
    
    * format
    
    * Add additional body model for smc_reader
    
    * SMCReader uses body_model's device
    
    * Remove warning
    
    * Add more unit tests for getting smpl by device
    
    * Remove unused import
    
    * Fix bug
    
    * Add unit test for body models
    
    * Move unit test for body model to test_body_models/
    
    * Add unit tests for transform_to_camera_frame
    
    * Clarify variable name
    
    * format
    
    * Fix SMCReader to avoid warning keep showing up
    
    * Fix smpl unit test
    
    * Remove unsupported unit test and bug fix
    
    * Relax tolerance

--- mmhuman3d/models/body_models/__init__.py
+++ mmhuman3d/models/body_models/__init__.py
@@ -2,5 +2,9 @@
 
 from .smpl import SMPL, GenderedSMPL, HybrIKSMPL
 from .smplx import SMPLX
+from .utils import batch_transform_to_camera_frame, transform_to_camera_frame
 
-__all__ = ['SMPL', 'GenderedSMPL', 'HybrIKSMPL', 'SMPLX']
+__all__ = [
+    'SMPL', 'GenderedSMPL', 'HybrIKSMPL', 'SMPLX', 'transform_to_camera_frame',
+    'batch_transform_to_camera_frame'
+]
