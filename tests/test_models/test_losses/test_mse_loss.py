da0ddf7b9aa73f6f63e86d0290b215bb17eddb9f
yl-1993 <yangleidev@gmail.com>
2022-03-25 17:15:48 +0800

    Move test_losses to test_models

--- tests/test_models/test_losses/test_mse_loss.py
+++ tests/test_models/test_losses/test_mse_loss.py
@@ -3,18 +3,6 @@ import torch
 from mmhuman3d.models import build_loss
 
 
-def test_mse_loss():
-    loss_cfg = dict(type='MSELoss')
-    loss = build_loss(loss_cfg)
-    fake_pred = torch.zeros((1, 3, 3))
-    fake_target = torch.zeros((1, 3, 3))
-    assert torch.allclose(loss(fake_pred, fake_target), torch.tensor(0.))
-
-    fake_pred = torch.ones((1, 3, 3))
-    fake_target = torch.zeros((1, 3, 3))
-    assert torch.allclose(loss(fake_pred, fake_target), torch.tensor(1.))
-
-
 def test_keypoint_mse__loss():
     loss_cfg = dict(type='KeypointMSELoss')
     loss = build_loss(loss_cfg)
