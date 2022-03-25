087ae9c3337c4726b79be3358da7d79e252f4583
yl-1993 <yangleidev@gmail.com>
2022-03-25 17:21:44 +0800

    Revise test_prior_losses

--- tests/test_models/test_losses/test_prior_losses.py
+++ tests/test_models/test_losses/test_prior_losses.py
@@ -2,13 +2,11 @@ import torch
 
 from mmhuman3d.models import build_loss
 
-batch_size = 2
-
 
 def test_shape_prior_loss():
     loss_config = dict(type='ShapePriorLoss', reduction='mean')
     loss = build_loss(loss_config)
-    betas = torch.rand((batch_size, 10))
+    betas = torch.rand(2, 10)
     output = loss(betas)
     assert isinstance(output, torch.Tensor) and \
            output.size() == ()
@@ -18,7 +16,7 @@ def test_joint_prior_loss():
     loss_config = dict(
         type='JointPriorLoss', use_full_body=False, reduction='mean')
     loss = build_loss(loss_config)
-    body_pose = torch.rand((batch_size, 69))
+    body_pose = torch.rand(2, 69)
     output = loss(body_pose)
     assert isinstance(output, torch.Tensor) and \
            output.size() == ()
@@ -27,7 +25,7 @@ def test_joint_prior_loss():
 def test_smooth_joint_loss():
     loss_config = dict(type='SmoothJointLoss', reduction='mean')
     loss = build_loss(loss_config)
-    body_pose = torch.rand((batch_size, 69))
+    body_pose = torch.rand(2, 69)
     output = loss(body_pose)
     assert isinstance(output, torch.Tensor) and \
            output.size() == ()
@@ -36,7 +34,7 @@ def test_smooth_joint_loss():
 def test_smooth_pelvis_loss():
     loss_config = dict(type='SmoothPelvisLoss', reduction='mean')
     loss = build_loss(loss_config)
-    global_orient = torch.rand((batch_size, 3))
+    global_orient = torch.rand(2, 3)
     output = loss(global_orient)
     assert isinstance(output, torch.Tensor) and \
            output.size() == ()
@@ -45,7 +43,7 @@ def test_smooth_pelvis_loss():
 def test_smooth_translation_loss():
     loss_config = dict(type='SmoothTranslationLoss', reduction='mean')
     loss = build_loss(loss_config)
-    transl = torch.rand((batch_size, 3))
+    transl = torch.rand(2, 3)
     output = loss(transl)
     assert isinstance(output, torch.Tensor) and \
            output.size() == ()
@@ -54,16 +52,7 @@ def test_smooth_translation_loss():
 def test_max_mixture_prior_loss():
     loss_config = dict(type='MaxMixturePrior', reduction='mean')
     loss = build_loss(loss_config)
-    body_pose = torch.rand((batch_size, 69))
+    body_pose = torch.rand(2, 69)
     output = loss(body_pose)
     assert isinstance(output, torch.Tensor) and \
            output.size() == ()
-
-
-if __name__ == '__main__':
-    test_shape_prior_loss()
-    test_joint_prior_loss()
-    test_smooth_joint_loss()
-    test_smooth_pelvis_loss()
-    test_smooth_translation_loss()
-    test_max_mixture_prior_loss()
