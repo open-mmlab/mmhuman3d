99df7fc5518d66891d50f6bda3296526809e4bf5
LengYue <lengyue@lengyue.me>
2022-03-30 23:48:22 -0400

    [Fix] Fix invalid samples filter (#140)
    * Fix invalid samples filter
    
    * lint

--- mmhuman3d/data/data_converters/mpi_inf_3dhp.py
+++ mmhuman3d/data/data_converters/mpi_inf_3dhp.py
@@ -53,13 +53,14 @@ class MpiInf3dhpConverter(BaseModeConverter):
         x_in = np.logical_and(keypoints2d[:, 0] < w, keypoints2d[:, 0] >= 0)
         y_in = np.logical_and(keypoints2d[:, 1] < h, keypoints2d[:, 1] >= 0)
         ok_pts = np.logical_and(x_in, y_in)
+
+        valid = True
         if np.sum(ok_pts) < num_keypoints:
             valid = False
 
         # add confidence column
         keypoints2d = np.hstack([keypoints2d, np.ones((num_keypoints, 1))])
         keypoints3d = np.hstack([keypoints3d, np.ones((num_keypoints, 1))])
-        valid = True
 
         return valid, keypoints2d, keypoints3d, bbox_xywh
 
