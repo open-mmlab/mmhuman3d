50ab9ffc6fe1413b11c23e6e77b9d49c687cb7d1
kristijanbartol <kristijan.bartol@gmail.com>
2022-04-13 08:04:01 +0200

    Support STAR model as an OpenMM model.

--- mmhuman3d/models/body_models/__init__.py
+++ mmhuman3d/models/body_models/__init__.py
@@ -1,10 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
+from star import STAR
+
 from .smpl import SMPL, GenderedSMPL, HybrIKSMPL
 from .smplx import SMPLX
 from .utils import batch_transform_to_camera_frame, transform_to_camera_frame
 
 __all__ = [
     'SMPL', 'GenderedSMPL', 'HybrIKSMPL', 'SMPLX', 'transform_to_camera_frame',
-    'batch_transform_to_camera_frame'
+    'batch_transform_to_camera_frame', 'STAR'
 ]
