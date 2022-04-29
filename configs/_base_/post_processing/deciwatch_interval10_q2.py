9b1c3b4ed68b213ff94a8fa9b18dec72a190b520
Xuan Ju <89566272+juxuan27@users.noreply.github.com>
2022-04-29 17:20:31 +0800

    Update deciwatch_interval10_q2.py

--- configs/_base_/post_processing/deciwatch_interval10_q2.py
+++ configs/_base_/post_processing/deciwatch_interval10_q2.py
@@ -5,7 +5,6 @@ speed_up_cfg = dict(
     type='deciwatch',
     interval=10,
     slide_window_q=2,
-    checkpoint='https://openmmlab-share.oss-cn-hangzhou.aliyuncs.com/mmhuman3d/\
-        models/deciwatch/deciwatch_interval10_q2.pth.tar?versionId=\
-            CAEQOhiBgICau8O9gxgiIDk1Y2Y0MzUxMmY0MDQzZThiYzhkMGJlMjc3ZDQ2NTQ2'
-)
+    checkpoint='https://openmmlab-share.oss-cn-hangzhou.aliyuncs.com/mmhuman3d/'
+        'models/deciwatch/deciwatch_interval10_q2.pth.tar?versionId='
+        'CAEQOhiBgICau8O9gxgiIDk1Y2Y0MzUxMmY0MDQzZThiYzhkMGJlMjc3ZDQ2NTQ2')
