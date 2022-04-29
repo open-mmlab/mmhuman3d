512856ba83db4b46a041aef4c0219720acad6866
Xuan Ju <89566272+juxuan27@users.noreply.github.com>
2022-04-29 17:20:57 +0800

    Update deciwatch_interval10_q2.py

--- configs/_base_/post_processing/deciwatch_interval10_q2.py
+++ configs/_base_/post_processing/deciwatch_interval10_q2.py
@@ -1,10 +1,10 @@
 # Config for DeciWatch trained on PW3D dataset with an interval of 10,
-# window size of 1 + 10*3(where q=3).
+# window size of 1 + 10*2(where q=2).
 # The model is trained only on SMPL pose parameters.
 speed_up_cfg = dict(
     type='deciwatch',
     interval=10,
-    slide_window_q=3,
+    slide_window_q=2,
     checkpoint='https://openmmlab-share.oss-cn-hangzhou.aliyuncs.com/mmhuman3d/'
-        'models/deciwatch/deciwatch_interval10_q3.pth.tar?versionId='
-        'CAEQOhiBgICIq8O9gxgiIDZiMjEzMjY3ODA4MTQwNGY5NTU3OWNkZjRjZjI2ZDFi')
+        'models/deciwatch/deciwatch_interval10_q2.pth.tar?versionId='
+        'CAEQOhiBgICau8O9gxgiIDk1Y2Y0MzUxMmY0MDQzZThiYzhkMGJlMjc3ZDQ2NTQ2')
