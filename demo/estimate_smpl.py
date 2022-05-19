0b7536d0433c75ff8223f63b62e4e2408d893a06
Xuan Ju <89566272+juxuan27@users.noreply.github.com>
2022-05-19 19:47:35 +0800

    Update estimate_smpl.py

--- demo/estimate_smpl.py
+++ demo/estimate_smpl.py
@@ -216,9 +216,12 @@ def single_person_with_mmdet(args, frames_iter):
     # smooth
     if args.smooth_type is not None:
         smpl_poses = smooth_process(
-            smpl_poses.reshape(frame_num, 24, 9), smooth_type=args.smooth_type)
+            smpl_poses.reshape(frame_num, 24, 9),
+            smooth_type=args.smooth_type).reshape(frame_num, 24, 3, 3)
         verts = smooth_process(verts, smooth_type=args.smooth_type)
-        smpl_poses = smpl_poses.reshape(frame_num, 24, 3, 3)
+        pred_cams = smooth_process(
+            pred_cams[:, np.newaxis],
+            smooth_type=args.smooth_type).reshape(frame_num, 3)
 
     if smpl_poses.shape[1:] == (24, 3, 3):
         smpl_poses = rotmat_to_aa(smpl_poses)
@@ -387,9 +390,11 @@ def multi_person_with_mmtracking(args, frames_iter):
     if args.smooth_type is not None:
         smpl_poses = smooth_process(
             smpl_poses.reshape(frame_num, -1, 24, 9),
-            smooth_type=args.smooth_type)
+            smooth_type=args.smooth_type).reshape(frame_num, -1, 24, 3, 3)
         verts = smooth_process(verts, smooth_type=args.smooth_type)
-        smpl_poses = smpl_poses.reshape(frame_num, -1, 24, 3, 3)
+        pred_cams = smooth_process(
+            pred_cams[:, np.newaxis],
+            smooth_type=args.smooth_type).reshape(frame_num, -1, 3)
 
     if smpl_poses.shape[2:] == (24, 3, 3):
         smpl_poses = rotmat_to_aa(smpl_poses)
