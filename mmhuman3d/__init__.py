df154d65efcdee713f25ab7de527fe4eaf6d86d1
sunqingping <sunqingpings@gmail.com>
2022-07-01 23:03:49 +0800

    fix mmcv_minimum_version

--- mmhuman3d/__init__.py
+++ mmhuman3d/__init__.py
@@ -15,7 +15,7 @@ def digit_version(version_str):
     return digit_version
 
 
-mmcv_minimum_version = '1.3.13'
+mmcv_minimum_version = '1.3.17'
 mmcv_maximum_version = '1.6.0'
 mmcv_version = digit_version(mmcv.__version__)
 
