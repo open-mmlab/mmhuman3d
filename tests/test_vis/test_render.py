54d79729396d292b365ee0ffd06355a456559b3d
sunqingping <sunqingpings@gmail.com>
2022-07-01 18:33:56 +0800

    add unittest

--- tests/test_vis/test_render.py
+++ tests/test_vis/test_render.py
@@ -1,8 +1,11 @@
+from unittest.mock import patch
+
 import numpy as np
 import pytest
 import torch
 from pytorch3d.renderer.mesh.textures import TexturesVertex
 from pytorch3d.utils import ico_sphere
+from surrogate import surrogate
 
 from mmhuman3d.core.cameras import compute_orbit_cameras
 from mmhuman3d.core.cameras.builder import build_cameras
@@ -63,7 +66,48 @@ def test_realtime_render_cuda():
     assert res.shape == (224, 224, 3)
 
 
-def test_realtime_render():
+@surrogate(
+    'mmhuman3d.core.renderer.mpr_renderer.cuda.rasterizer.estimate_normals')
+@patch('mmhuman3d.core.renderer.mpr_renderer.cuda.rasterizer.estimate_normals')
+@surrogate('mmhuman3d.core.renderer.mpr_renderer.cuda.rasterizer.project_mesh')
+@patch('mmhuman3d.core.renderer.mpr_renderer.cuda.rasterizer.project_mesh')
+def test_realtime_render(mock_project_mesh, mock_estimate_normals):
+    mock_estimate_normals.return_value = np.zeros([1024, 1024, 3]), np.zeros(
+        [1024, 1024, 3])
+    mock_project_mesh.return_value = np.zeros([1024, 1024, 1])
+    # test realrender
+    from mmhuman3d.core.renderer.mpr_renderer.smpl_realrender import VisualizerMeshSMPL  # noqa: E501
+    with pytest.raises(AssertionError):
+        vertices = torch.ones([6890, 3]).to(device='cpu')
+        body_model = build_body_model(
+            dict(
+                type='SMPL',
+                gender='neutral',
+                num_betas=10,
+                model_path='data/body_models/smpl'))
+        renderer = VisualizerMeshSMPL(
+            body_models=body_model, resolution=[224, 224], device='cpu')
+
+        res = renderer(vertices)
+
+    # test estimate normals
+    from mmhuman3d.core.renderer.mpr_renderer.rasterizer import \
+        estimate_normals
+    vertices = torch.zeros([6890, 3])
+    vertices_filter = torch.ones(6890)
+    faces = torch.zeros([13776, 3])
+    pinhole2d = Pinhole2D(fx=5000., fy=5000., cx=112, cy=112, w=1024, h=1024)
+    coords, normals = estimate_normals(vertices, faces, pinhole2d,
+                                       vertices_filter)
+    assert coords.shape == (1024, 1024, 3)
+
+    # test project mesh
+    from mmhuman3d.core.renderer.mpr_renderer.rasterizer import \
+        project_mesh
+    z_buff = project_mesh(vertices, faces, vertices, pinhole2d,
+                          vertices_filter)
+    assert z_buff.shape == (1024, 1024, 1)
+
     # test camera
     pinhole2d = Pinhole2D(fx=5000., fy=5000., cx=112, cy=112, w=1024, h=1024)
     K = pinhole2d.get_K()
@@ -72,3 +116,24 @@ def test_realtime_render():
     verts = torch.ones([6890, 3])
     verts_ndc = pinhole2d.project_ndc(verts)
     assert verts_ndc.shape == (6890, 3)
+
+    # test utils
+    from mmhuman3d.core.renderer.mpr_renderer.utils import (vis_z_buffer,
+                                                            vis_normals)
+    z_ones = torch.ones([1024, 1024, 1])
+    z_zeros = torch.zeros([1024, 1024, 1])
+    coords = torch.zeros([1024, 1024, 3])
+    normals = torch.zeros([1024, 1024, 3])
+
+    res = vis_z_buffer(z_zeros)
+    assert res.shape == (1024, 1024)
+
+    res = vis_z_buffer(z_ones)
+    assert res.shape == (1024, 1024)
+
+    res = vis_normals(coords, normals)
+    assert res.shape == (1024, 1024)
+
+
+if __name__ == '__main__':
+    test_realtime_render()
