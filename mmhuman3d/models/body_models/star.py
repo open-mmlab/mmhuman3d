0789eb7eecfad3bb7d227a879fb45f85cbb0052d
kristijanbartol <kristijan.bartol@gmail.com>
2022-07-04 17:40:48 +0200

    Update STAR to use keypoint conversion. Move additional licenses.

--- mmhuman3d/models/body_models/star.py
+++ mmhuman3d/models/body_models/star.py
@@ -1,27 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
-# Adapted from:
-#
-# https://github.com/ahmedosman/STAR/blob/master/star/pytorch/star.py
-#
-#
-# -*- coding: utf-8 -*-
-#
-# LICENSE:
-# Max-Planck-Gesellschaft zur Förderung der Wissenschaften e.V. (MPG) is
-# holder of all proprietary rights on this computer program.
-# You can only use this computer program if you have closed
-# a license agreement with MPG or you get the right to use the computer
-# program from someone who is authorized to grant you that right.
-# Any use of the computer program without a valid license is prohibited and
-# liable to prosecution.
-#
-# Copyright©2019 Max-Planck-Gesellschaft zur Förderung
-# der Wissenschaften e.V. (MPG). acting on behalf of its Max Planck Institute
-# for Intelligent Systems. All rights reserved.
-#
-# Contact: ps-license@tuebingen.mpg.de
-
 import os
 from typing import Optional
 
@@ -29,27 +7,85 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-from mmhuman3d.utils.transforms import quat_feat, rodrigues, with_zeros
+from mmhuman3d.core.conventions.keypoints_mapping import convert_kps
+from mmhuman3d.utils.transforms import (
+    aa_to_rotmat,
+    make_homegeneous_rotmat_batch,
+)
 
 
 class STAR(nn.Module):
 
+    NUM_BODY_JOINTS = 23
+
     def __init__(self,
                  model_path: str,
                  gender: str = 'neutral',
-                 num_betas: int = 10) -> None:
+                 keypoint_src: str = 'star',
+                 keypoint_dst: str = 'human_data',
+                 keypoint_approximate: bool = False,
+                 create_global_orient: bool = True,
+                 global_orient: Optional[torch.Tensor] = None,
+                 create_body_pose: bool = True,
+                 body_pose: torch.Tensor = None,
+                 num_betas: int = 10,
+                 create_betas: bool = True,
+                 betas: torch.Tensor = None,
+                 create_trans: bool = True,
+                 trans: torch.Tensor = None,
+                 batch_size: int = 1,
+                 dtype=torch.float32) -> None:
         """STAR model constructor.
 
         Parameters
         ----------
         model_path: str
             The path to the folder or to the file where the model
-            parameters are stored
+            parameters are stored.
+        gender: str, optional
+            Which gender to load.
+        keypoint_src: str
+            Source convention of keypoints. This convention is used for
+            keypoints obtained from joint regressors. Keypoints then
+            undergo  conversion into keypoint_dst convention.
+        keypoint_dst: destination convention of keypoints. This convention
+            is used for keypoints in the output.
+        keypoint_approximate: whether to use approximate matching in
+            convention conversion for keypoints.
+        create_global_orient: bool, optional
+            Flag for creating a member variable for the global orientation
+            of the body. (default = True)
+        global_orient: torch.tensor, optional, Bx3
+            The default value for the global orientation variable.
+            (default = None)
+        create_body_pose: bool, optional
+            Flag for creating a member variable for the pose of the body.
+            (default = True)
+        body_pose: torch.tensor, optional, Bx(Body Joints * 3)
+            The default value for the body pose variable.
+            (default = None)
         num_betas: int, optional
             Number of shape components to use
             (default = 10).
-        gender: str, optional
-            Which gender to load
+        create_betas: bool, optional
+            Flag for creating a member variable for the shape space
+            (default = True).
+        betas: torch.tensor, optional, Bx10
+            The default value for the shape member variable.
+            (default = None)
+        create_trans: bool, optional
+            Flag for creating a member variable for the translation
+            of the body. (default = True)
+        trans: torch.tensor, optional, Bx3
+            The default value for the trans variable.
+            (default = None)
+        dtype: torch.dtype, optional
+                The data type for the created variables.
+        batch_size: int, optional
+            The batch size used for creating the member variables.
+        num_betas: int, optional
+            Number of shape components to use
+            (default = 10).
         """
         if gender not in ['male', 'female', 'neutral']:
             raise RuntimeError('Invalid gender! Should be one of '
@@ -69,6 +105,10 @@ class STAR(nn.Module):
             star_path = model_path
 
         super(STAR, self).__init__()
+
+        self.keypoint_src = keypoint_src
+        self.keypoint_dst = keypoint_dst
+        self.keypoint_approximate = keypoint_approximate
 
         star_model = np.load(star_path, allow_pickle=True)
         J_regressor = star_model['J_regressor']
@@ -121,36 +161,101 @@ class STAR(nn.Module):
             ],
                          dtype=torch.int64))
 
+        if create_global_orient:
+            if global_orient is None:
+                default_global_orient = torch.zeros([batch_size, 3],
+                                                    dtype=dtype)
+            else:
+                if torch.is_tensor(global_orient):
+                    default_global_orient = global_orient.clone().detach()
+                else:
+                    default_global_orient = torch.tensor(
+                        global_orient, dtype=dtype)
+
+            global_orient = nn.Parameter(
+                default_global_orient, requires_grad=True)
+            self.register_parameter('global_orient', global_orient)
+
+        if create_body_pose:
+            if body_pose is None:
+                default_body_pose = torch.zeros(
+                    [batch_size, self.NUM_BODY_JOINTS * 3], dtype=dtype)
+            else:
+                if torch.is_tensor(body_pose):
+                    default_body_pose = body_pose.clone().detach()
+                else:
+                    default_body_pose = torch.tensor(body_pose, dtype=dtype)
+            self.register_parameter(
+                'body_pose',
+                nn.Parameter(default_body_pose, requires_grad=True))
+
+        if create_betas:
+            if betas is None:
+                default_betas = torch.zeros([batch_size, self.num_betas],
+                                            dtype=dtype)
+            else:
+                if torch.is_tensor(betas):
+                    default_betas = betas.clone().detach()
+                else:
+                    default_betas = torch.tensor(betas, dtype=dtype)
+
+            self.register_parameter(
+                'betas', nn.Parameter(default_betas, requires_grad=True))
+
+        if create_trans:
+            if trans is None:
+                default_trans = torch.zeros([batch_size, 3],
+                                            dtype=dtype,
+                                            requires_grad=True)
+            else:
+                default_trans = torch.tensor(trans, dtype=dtype)
+            self.register_parameter(
+                'trans', nn.Parameter(default_trans, requires_grad=True))
+
         self.verts = None
         self.J = None
         self.R = None
 
     def forward(self,
-                pose: Optional[torch.Tensor] = None,
+                global_orient: Optional[torch.Tensor] = None,
+                body_pose: Optional[torch.Tensor] = None,
                 betas: Optional[torch.Tensor] = None,
-                trans: Optional[torch.Tensor] = None) -> torch.Tensor:
-        """Forward pass for the SMPL model.
+                trans: Optional[torch.Tensor] = None,
+                return_verts: bool = True,
+                return_full_pose: bool = True) -> torch.Tensor:
+        """Forward pass for the STAR model.
+
         Parameters
         ----------
-        :param pose: pose  parameters - A batch size x 72 tensor
-            (3 numbers for each joint)
-        :param beta: beta  parameters - A batch size x number of betas
-        :param beta: trans parameters - A batch size x 3
-        :return:
-             v         : batch size x 6890 x 3
-                         The STAR model vertices
-             v.v_vposed: batch size x 6890 x 3 model
-                         STAR vertices in T-pose after adding the shape
-                         blend shapes and pose blend shapes
-             v.v_shaped: batch size x 6890 x 3
-                         STAR vertices in T-pose after adding the shape
-                         blend shapes and pose blend shapes
-             v.J_transformed:batch size x 24 x 3
-                            Posed model joints.
-             v.f: A numpy array of the model face.
+        global_orient: torch.tensor, optional, shape Bx3
+            Global orientation (rotation) of the body. If given, ignore the
+            member variable and use it as the global rotation of the body.
+            Useful if someone wishes to predicts this with an external model.
+            (default=None)
+        body_pose: torch.Tensor, shape Bx(J*3)
+            Pose parameters for the STAR model. It should be a tensor that
+            contains joint rotations in axis-angle format. If given, ignore
+            the member variable and use it as the body parameters.
+            (default=None)
+        betas: torch.Tensor, shape Bx10
+            Shape parameters for the STAR model. If given, ignore the member
+            variable and use it as shape parameters. (default=None)
+        trans: torch.Tensor, shape Bx3
+            Translation vector for the STAR model. If given, ignore the member
+            variable and use it as the translation of the body. (default=None)
+        Returns:
+            output: Contains output parameters and attributes corresponding
+            to other body models.
         """
-        device = pose.device
-        batch_size = pose.shape[0]
+        global_orient = (
+            global_orient if global_orient is not None else self.global_orient)
+        body_pose = body_pose if body_pose is not None else self.body_pose
+        betas = betas if betas is not None else self.betas
+        apply_trans = trans is not None or hasattr(self, 'trans')
+        if trans is None and hasattr(self, 'transl'):
+            trans = self.trans
+
+        batch_size = body_pose.shape[0]
         v_template = self.v_template[None, :]
         shapedirs = self.shapedirs.view(-1, self.num_betas)[None, :].expand(
             batch_size, -1, -1)
@@ -158,55 +263,76 @@ class STAR(nn.Module):
         v_shaped = torch.matmul(shapedirs, beta).view(-1, 6890, 3) + v_template
         J = torch.einsum('bik,ji->bjk', [v_shaped, self.J_regressor])
 
-        pose_quat = quat_feat(pose.view(-1, 3)).view(batch_size, -1)
+        pose_quat = self.normalize_quaternion(body_pose.view(-1, 3)).view(
+            batch_size, -1)
         pose_feat = torch.cat((pose_quat[:, 4:], beta[:, 1]), 1)
 
-        R = rodrigues(pose.view(-1, 3)).view(batch_size, 24, 3, 3)
+        R = aa_to_rotmat(body_pose.view(-1, 3)).view(batch_size, 24, 3, 3)
         R = R.view(batch_size, 24, 3, 3)
 
         posedirs = self.posedirs[None, :].expand(batch_size, -1, -1)
         v_posed = v_shaped + torch.matmul(
             posedirs, pose_feat[:, :, None]).view(-1, 6890, 3)
 
-        J_ = J.clone()
-        J_[:, 1:, :] = J[:, 1:, :] - J[:, self.parent, :]
-        G_ = torch.cat([R, J_[:, :, :, None]], dim=-1)
-        pad_row = torch.FloatTensor([0, 0, 0,
-                                     1]).to(device).view(1, 1, 1, 4).expand(
-                                         batch_size, 24, -1, -1)
-        G_ = torch.cat([G_, pad_row], dim=2)
-        G = [G_[:, 0].clone()]
-        for i in range(1, 24):
-            G.append(torch.matmul(G[self.parent[i - 1]], G_[:, i, :, :]))
-        G = torch.stack(G, dim=1)
-
-        rest = torch.cat([J, torch.zeros(batch_size, 24, 1).to(device)],
-                         dim=2).view(batch_size, 24, 4, 1)
-        zeros = torch.zeros(batch_size, 24, 4, 3).to(device)
-        rest = torch.cat([zeros, rest], dim=-1)
-        rest = torch.matmul(G, rest)
-        G = G - rest
-        T = torch.matmul(self.weights,
-                         G.permute(1, 0, 2, 3).contiguous().view(24, -1)).view(
-                             6890, batch_size, 4, 4).transpose(0, 1)
-        rest_shape_h = torch.cat(
-            [v_posed, torch.ones_like(v_posed)[:, :, [0]]], dim=-1)
-        v = torch.matmul(T, rest_shape_h[:, :, :, None])[:, :, :3, 0]
-        v = v + trans[:, None, :]
-        v.f = self.f
-        v.v_posed = v_posed
-        v.v_shaped = v_shaped
-
-        root_transform = with_zeros(
+        root_transform = make_homegeneous_rotmat_batch(
             torch.cat((R[:, 0], J[:, 0][:, :, None]), 2))
         results = [root_transform]
         for i in range(0, self.parent.shape[0]):
-            transform_i = with_zeros(
+            transform_i = make_homegeneous_rotmat_batch(
                 torch.cat((R[:, i + 1], J[:, i + 1][:, :, None] -
                            J[:, self.parent[i]][:, :, None]), 2))
             curr_res = torch.matmul(results[self.parent[i]], transform_i)
             results.append(curr_res)
         results = torch.stack(results, dim=1)
         posed_joints = results[:, :, :3, 3]
-        v.J_transformed = posed_joints + trans[:, None, :]
-        return v
+
+        if apply_trans:
+            posed_joints += trans[:, None, :]
+            v_posed += trans[:, None, :]
+
+        joints, joint_mask = convert_kps(
+            posed_joints,
+            src=self.keypoint_src,
+            dst=self.keypoint_dst,
+            approximate=self.keypoint_approximate)
+        if isinstance(joint_mask, np.ndarray):
+            joint_mask = torch.tensor(
+                joint_mask, dtype=torch.uint8, device=joints.device)
+
+        joint_mask = joint_mask.reshape(1, -1).expand(batch_size, -1)
+
+        output = dict(
+            global_orient=global_orient,
+            body_pose=body_pose,
+            joints=posed_joints,
+            joint_mask=joint_mask,
+            keypoints=torch.cat([joints, joint_mask[:, :, None]], dim=-1),
+            betas=beta)
+
+        if return_verts:
+            output['vertices'] = v_posed
+        if return_full_pose:
+            output['full_pose'] = torch.cat([global_orient, body_pose], dim=1)
+
+        return output
+
+    @classmethod
+    def normalize_quaternion(self, theta: torch.Tensor) -> torch.Tensor:
+        """Computes a normalized quaternion ([0,0,0,0] when the body is in rest
+        pose) given joint angles.
+
+        Args:
+            theta (torch.Tensor): A tensor of joints axis angles,
+                batch size x number of joints x 3
+
+        Returns:
+            quat (torch.Tensor)
+        """
+        l1norm = torch.norm(theta + 1e-8, p=2, dim=1)
+        angle = torch.unsqueeze(l1norm, -1)
+        normalized = torch.div(theta, angle)
+        angle = angle * 0.5
+        v_cos = torch.cos(angle)
+        v_sin = torch.sin(angle)
+        quat = torch.cat([v_sin * normalized, v_cos - 1], dim=1)
+        return quat
--- mmhuman3d/utils/transforms.py
+++ mmhuman3d/utils/transforms.py
@@ -548,6 +548,59 @@ def sja_to_aa(
     return t(sja, R_t=R_t, R_t_inv=R_t_inv)
 
 
+def transform_to_camera_frame(global_orient, transl, pelvis, extrinsic):
+    """Transform body model parameters to camera frame.
+
+    Args:
+        global_orient (numpy.ndarray): shape (3, ). Only global_orient and
+            transl needs to be updated in the rigid transformation
+        transl (numpy.ndarray): shape (3, ).
+        pelvis (numpy.ndarray): shape (3, ). 3D joint location of pelvis
+            This is necessary to eliminate the offset from SMPL
+            canonical space origin to pelvis, because the global orient
+            is conducted around the pelvis, not the canonical space origin
+        extrinsic (numpy.ndarray): shape (4, 4). Transformation matrix
+            from world frame to camera frame
+    Returns:
+        (new_gloabl_orient, new_transl)
+            new_gloabl_orient: transformed global orient
+            new_transl: transformed transl
+    """
+
+    # take out the small offset from smpl origin to pelvis
+    transl_offset = pelvis - transl
+    T_p2w = numpy.eye(4)
+    T_p2w[:3, 3] = transl_offset
+
+    # camera extrinsic: transformation from world frame to camera frame
+    T_w2c = extrinsic
+
+    # smpl transformation: from vertex frame to world frame
+    T_v2p = numpy.eye(4)
+    global_orient_mat = aa_to_rotmat(global_orient)
+    T_v2p[:3, :3] = global_orient_mat
+    T_v2p[:3, 3] = transl
+
+    # compute combined transformation from vertex to world
+    T_v2w = T_p2w @ T_v2p
+
+    # compute transformation from vertex to camera
+    T_v2c = T_w2c @ T_v2w
+
+    # decompose vertex to camera transformation
+    # np: new pelvis frame
+    # T_v2c = T_np2c x T_v2np
+    T_np2c = T_p2w
+    T_v2np = numpy.linalg.inv(T_np2c) @ T_v2c
+
+    # decompose into new global orient and new transl
+    new_global_orient_mat = T_v2np[:3, :3]
+    new_global_orient = rotmat_to_aa(new_global_orient_mat)
+    new_transl = T_v2np[:3, 3]
+
+    return new_global_orient, new_transl
+
+
 def batch_transform_to_camera_frame(global_orient, transl, pelvis, extrinsic):
     """Transform body model parameters to camera frame by batch.
 
@@ -599,81 +652,16 @@ def batch_transform_to_camera_frame(global_orient, transl, pelvis, extrinsic):
 
     # decompose into new global orient and new transl
     new_global_orient_mat = T_v2np[:, :3, :3]
-    new_gloabl_orient = rotmat_to_aa(new_global_orient_mat)
+    new_global_orient = rotmat_to_aa(new_global_orient_mat)
     new_transl = T_v2np[:, :3, 3]
 
-    assert new_gloabl_orient.shape == (N, 3)
+    assert new_global_orient.shape == (N, 3)
     assert new_transl.shape == (N, 3)
 
-    return new_gloabl_orient, new_transl
+    return new_global_orient, new_transl
 
 
-def quat_feat(theta: torch.Tensor) -> torch.Tensor:
-    """Computes a normalized quaternion ([0,0,0,0]  when the body is in rest
-    pose) given joint angles.
-
-    Args:
-        theta (torch.Tensor): A tensor of joints axis angles,
-            batch size x number of joints x 3
-
-    Returns:
-        quat (torch.Tensor)
-    """
-    l1norm = torch.norm(theta + 1e-8, p=2, dim=1)
-    angle = torch.unsqueeze(l1norm, -1)
-    normalized = torch.div(theta, angle)
-    angle = angle * 0.5
-    v_cos = torch.cos(angle)
-    v_sin = torch.sin(angle)
-    quat = torch.cat([v_sin * normalized, v_cos - 1], dim=1)
-    return quat
-
-
-def _quat2mat(quat: torch.Tensor) -> torch.Tensor:
-    """Converts a quaternion to a rotation matrix.
-
-    Args:
-        quat (torch.Tensor)
-
-    Returns:
-        rotMat (torch.Tensor)
-    """
-    norm_quat = quat
-    norm_quat = norm_quat / norm_quat.norm(p=2, dim=1, keepdim=True)
-    w, x, y, z = norm_quat[:, 0], norm_quat[:, 1], norm_quat[:, 2], \
-        norm_quat[:, 3]
-    B = quat.size(0)
-    w2, x2, y2, z2 = w.pow(2), x.pow(2), y.pow(2), z.pow(2)
-    wx, wy, wz = w * x, w * y, w * z
-    xy, xz, yz = x * y, x * z, y * z
-    rotMat = torch.stack([
-        w2 + x2 - y2 - z2, 2 * xy - 2 * wz, 2 * wy + 2 * xz, 2 * wz + 2 * xy,
-        w2 - x2 + y2 - z2, 2 * yz - 2 * wx, 2 * xz - 2 * wy, 2 * wx + 2 * yz,
-        w2 - x2 - y2 + z2
-    ],
-                         dim=1).view(B, 3, 3)
-    return rotMat
-
-
-def rodrigues(theta: torch.Tensor) -> torch.Tensor:
-    """Computes the rodrigues representation given joint angles.
-
-    Parameters
-    ----------
-    :param theta: batch_size x number of joints x 3
-    :return: batch_size x number of joints x 3 x 4
-    """
-    l1norm = torch.norm(theta + 1e-8, p=2, dim=1)
-    angle = torch.unsqueeze(l1norm, -1)
-    normalized = torch.div(theta, angle)
-    angle = angle * 0.5
-    v_cos = torch.cos(angle)
-    v_sin = torch.sin(angle)
-    quat = torch.cat([v_cos, v_sin * normalized], dim=1)
-    return _quat2mat(quat)
-
-
-def with_zeros(input: torch.Tensor) -> torch.Tensor:
+def make_homegeneous_rotmat_batch(input: torch.Tensor) -> torch.Tensor:
     """Appends a row of [0,0,0,1] to a batch size x 3 x 4 Tensor.
 
     Parameters
@@ -685,5 +673,19 @@ def with_zeros(input: torch.Tensor) -> torch.Tensor:
     row_append = torch.tensor([0.0, 0.0, 0.0, 1.0], dtype=torch.float)
     row_append.requires_grad = False
     padded_tensor = torch.cat(
-        [input, row_append.view(1, 1, 4).repeat(batch_size, 1, 1)], 1)
+        [input, row_append.view(1, 1, 4).repeat(batch_size, 1, 1)], dim=1)
+    return padded_tensor
+
+
+def make_homegeneous_rotmat(input: torch.Tensor) -> torch.Tensor:
+    """Appends a row of [0,0,0,1] to a 3 x 4 Tensor.
+
+    Parameters
+    ----------
+    :param input: A tensor of dimensions 3 x 4
+    :return: A tensor batch size x 4 x 4 (appended with 0,0,0,1)
+    """
+    row_append = torch.tensor([0.0, 0.0, 0.0, 1.0], dtype=torch.float)
+    row_append.requires_grad = False
+    padded_tensor = torch.cat(input, row_append, dim=1)
     return padded_tensor
