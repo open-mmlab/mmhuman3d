c4faa516880893a2e9f85ebbe1de6e024ba410c3
shizhelun <shizhelun20000820@126.com>
2022-07-06 21:00:45 +0800

    [Feature]smplx dataset unittest

--- configs/expose/hrnet_hmr_expose_body.py
+++ configs/expose/hrnet_hmr_expose_body.py
@@ -11,7 +11,7 @@ optimizer = dict(
 optimizer_config = dict(grad_clip=None)
 lr_config = dict(policy='step',step=[60, 100],gamma = 0.1)
 
-runner = dict(type='EpochBasedRunner', max_epochs=120)
+runner = dict(type='EpochBasedRunner', max_epochs=100)
 
 log_config = dict(
     interval=50, hooks=[
