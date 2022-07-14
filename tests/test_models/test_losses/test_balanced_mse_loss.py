57c97167ad9e880e6756c4c0ee76eb7653c08cee
mingyuan-zhang <zhangmy718@gmail.com>
2022-07-14 09:42:24 +0800

    remove all_gather test

--- tests/test_models/test_losses/test_balanced_mse_loss.py
+++ tests/test_models/test_losses/test_balanced_mse_loss.py
@@ -16,19 +16,6 @@ def test_balanced_mse_loss():
     target = torch.zeros(1, 3, 2)
     assert torch.allclose(loss(pred, target), torch.tensor(0.5))
 
-    from mmcv.runner import init_dist
-    import os
-    os.environ['RANK'] = '0'
-    os.environ['WORLD_SIZE'] = '1'
-    os.environ['MASTER_ADDR'] = '127.0.0.1'
-    os.environ['MASTER_PORT'] = '29500'
-    init_dist('pytorch')
-    loss_cfg = dict(type='BMCLossMD', all_gather=True)
-    loss = build_loss(loss_cfg)
-    pred = torch.ones(1, 3, 2).cuda()
-    target = torch.zeros(1, 3, 2).cuda()
-    assert torch.allclose(loss(pred, target), torch.tensor(0.5))
-
     loss_cfg = dict(type='BMCLossMD', init_noise_sigma=2.0)
     loss = build_loss(loss_cfg)
     pred = torch.ones(1, 3, 2)
