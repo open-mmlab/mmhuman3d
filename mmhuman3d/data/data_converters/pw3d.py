71fb05fbaff083eeb46f87524568ded13b3fb8d5
pangyyyyy <pangyyyyyy96@gmail.com>
2022-09-07 11:15:34 +0800

    update pw3d train data converter

--- mmhuman3d/data/data_converters/pw3d.py
+++ mmhuman3d/data/data_converters/pw3d.py
@@ -5,11 +5,14 @@ from typing import List
 import cv2
 import numpy as np
 from tqdm import tqdm
+import torch
 
+from mmhuman3d.core.conventions.keypoints_mapping import convert_kps
 from mmhuman3d.core.cameras.camera_parameters import CameraParameter
 from mmhuman3d.data.data_converters.builder import DATA_CONVERTERS
 from mmhuman3d.data.data_structures.human_data import HumanData
 from .base_converter import BaseModeConverter
+from mmhuman3d.models.body_models.builder import build_body_model
 
 
 @DATA_CONVERTERS.register_module()
@@ -48,11 +51,13 @@ class Pw3dConverter(BaseModeConverter):
         human_data = HumanData()
 
         # structs we use
-        image_path_, bbox_xywh_, cam_param_ = [], [], []
+        image_path_, bbox_xywh_, cam_param_, keypoints2d_, keypoints3d_ = \
+            [], [], [], [], []
         smpl = {}
         smpl['body_pose'] = []
         smpl['global_orient'] = []
         smpl['betas'] = []
+        smpl['transl'] = []
         meta = {}
         meta['gender'] = []
 
@@ -64,6 +69,16 @@ class Pw3dConverter(BaseModeConverter):
             if f.endswith('.pkl')
         ]
 
+        # # # get smpl layer
+        body_model = dict(
+            type='GenderedSMPL',
+            keypoint_src='h36m',
+            keypoint_dst='h36m',
+            model_path='data/body_models/smpl',
+            joints_regressor='data/body_models/J_regressor_h36m.npy')
+        smpl_model = build_body_model(body_model)
+        gender_dict = {'f': 1, 'm': 0}
+
         # go through all the .pkl files
         for filename in tqdm(files):
             with open(filename, 'rb') as f:
@@ -71,8 +86,10 @@ class Pw3dConverter(BaseModeConverter):
                 smpl_pose = data['poses']
                 smpl_betas = data['betas']
                 poses2d = data['poses2d']
+                poses3d = data['jointPositions']
                 global_poses = data['cam_poses']
                 genders = data['genders']
+                trans = data['trans']
                 valid = np.array(data['campose_valid']).astype(np.bool)
                 K = np.array(data['cam_intrinsics'])
                 num_people = len(smpl_pose)
@@ -89,14 +106,22 @@ class Pw3dConverter(BaseModeConverter):
                                           (num_frames, 1))
                     valid_betas = valid_betas[valid[i]]
                     valid_keypoints_2d = poses2d[i][valid[i]]
+                    valid_keypoints_3d = poses3d[i][valid[i]]
                     valid_img_names = img_names[valid[i]]
                     valid_global_poses = global_poses[valid[i]]
                     gender = genders[i]
+                    t_s = trans[i]
 
                     # consider only valid frames
                     for valid_i in range(valid_pose.shape[0]):
                         keypoints2d = valid_keypoints_2d[valid_i, :, :].T
-                        keypoints2d = keypoints2d[keypoints2d[:, 2] > 0, :]
+                        # set min confdence to anything >0.1
+                        keypoints2d[keypoints2d[:, 2] > 0.3, 2] = 1
+                        # keypoints2d = keypoints2d[keypoints2d[:, 2] > 0, :]
+                        keypoints3d = valid_keypoints_3d[valid_i, :].reshape(
+                            24, 3)
+                        # keypoints3d -= keypoints3d[0]
+
                         bbox_xyxy = [
                             min(keypoints2d[:, 0]),
                             min(keypoints2d[:, 1]),
@@ -114,6 +139,8 @@ class Pw3dConverter(BaseModeConverter):
 
                         # transform global pose
                         pose = valid_pose[valid_i]
+                        beta = valid_betas[valid_i]
+                        transl = t_s[valid_i]
                         extrinsic_param = valid_global_poses[valid_i]
                         R = extrinsic_param[:3, :3]
                         T = extrinsic_param[:3, 3]
@@ -121,18 +148,47 @@ class Pw3dConverter(BaseModeConverter):
                         camera = CameraParameter(H=h, W=w)
                         camera.set_KRT(K, R, T)
                         parameter_dict = camera.to_dict()
-
                         pose[:3] = cv2.Rodrigues(
                             np.dot(R,
                                    cv2.Rodrigues(pose[:3])[0]))[0].T[0]
+
+                        # keypoints3d = np.concatenate(
+                        #     [keypoints3d, np.ones((24, 1))], axis=1)
 
                         image_path_.append(image_path)
                         bbox_xywh_.append(bbox_xywh)
                         smpl['body_pose'].append(pose[3:].reshape((23, 3)))
                         smpl['global_orient'].append(pose[:3])
-                        smpl['betas'].append(valid_betas[valid_i])
+                        smpl['betas'].append(beta)
+                        smpl['transl'].append(transl)
                         meta['gender'].append(gender)
                         cam_param_.append(parameter_dict)
+                        keypoints2d_.append(keypoints2d)
+
+                        pose = np.array(pose.reshape(1, 72), dtype=np.float32)
+                        betas = np.array(
+                            beta.reshape(1, 10), dtype=np.float32)
+                        transl = np.array(
+                            transl.reshape(1, 3), dtype=np.float32)
+
+                        device = 'cpu'
+                        output = smpl_model(
+                            pose_skeleton=torch.from_numpy(pose).to(device),
+                            betas=torch.from_numpy(betas).to(device),
+                            transl=torch.from_numpy(transl).to(device),
+                            phis=None,
+                            global_orient=None,
+                            gender=torch.Tensor([gender_dict[gender]]).to(
+                                device),  # add input for gender
+                            return_verts=True)
+
+                        keypoints3d = output['joints'].detach(
+                        ).numpy().squeeze()
+                        keypoints3d -= keypoints3d[0]
+
+                        keypoints3d = np.concatenate(
+                            [keypoints3d, np.ones((17, 1))], axis=1)
+                        keypoints3d_.append(keypoints3d)
 
         # change list to np array
         bbox_xywh_ = np.array(bbox_xywh_).reshape((-1, 4))
@@ -141,19 +197,31 @@ class Pw3dConverter(BaseModeConverter):
         smpl['global_orient'] = np.array(smpl['global_orient']).reshape(
             (-1, 3))
         smpl['betas'] = np.array(smpl['betas']).reshape((-1, 10))
+        smpl['transl'] = np.array(smpl['transl']).reshape((-1, 3))
         meta['gender'] = np.array(meta['gender'])
+        keypoints2d_ = np.array(keypoints2d_).reshape((-1, 18, 3))
+        keypoints2d_, keypoints2d_mask = convert_kps(keypoints2d_, 'pw3d',
+                                                     'human_data')
+        keypoints3d_ = np.array(keypoints3d_).reshape((-1, 17, 4))
+        keypoints3d_, keypoints3d_mask = convert_kps(keypoints3d_, 'h36m',
+                                                     'human_data')
 
         human_data['image_path'] = image_path_
         human_data['bbox_xywh'] = bbox_xywh_
+        human_data['keypoints2d_mask'] = keypoints2d_mask
+        human_data['keypoints3d_mask'] = keypoints3d_mask
+        human_data['keypoints2d'] = keypoints2d_
+        human_data['keypoints3d'] = keypoints3d_
         human_data['smpl'] = smpl
         human_data['meta'] = meta
         human_data['cam_param'] = cam_param_
         human_data['config'] = 'pw3d'
+        human_data.compress_keypoints_by_mask()
 
         # store data
         if not os.path.isdir(out_path):
             os.makedirs(out_path)
 
-        file_name = 'pw3d_{}.npz'.format(mode)
+        file_name = 'pw3d_{}_regressed.npz'.format(mode)
         out_file = os.path.join(out_path, file_name)
         human_data.dump(out_file)
