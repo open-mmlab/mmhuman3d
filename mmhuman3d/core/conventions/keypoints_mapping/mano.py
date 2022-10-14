612f8329c20d4332fa633b2a7777f92a2df88fab
yl-1993 <yangleidev@gmail.com>
2022-10-15 00:32:02 +0800

    Clean the code and add unittest

--- mmhuman3d/core/conventions/keypoints_mapping/mano.py
+++ mmhuman3d/core/conventions/keypoints_mapping/mano.py
@@ -1,11 +1,4 @@
-MANO_LEFT_KEYPOINTS = [
-    'left_wrist', 'left_index_1', 'left_index_2', 'left_index_3',
-    'left_middle_1', 'left_middle_2', 'left_middle_3', 'left_pinky_1',
-    'left_pinky_2', 'left_pinky_3', 'left_ring_1', 'left_ring_2',
-    'left_ring_3', 'left_thumb_1', 'left_thumb_2', 'left_thumb_3',
-    'left_thumb', 'left_index', 'left_middle', 'left_ring', 'left_pinky'
-]
-
+# Original order from MANO J_regressor
 MANO_RIGHT_KEYPOINTS = [
     'right_wrist', 'right_index_1', 'right_index_2', 'right_index_3',
     'right_middle_1', 'right_middle_2', 'right_middle_3', 'right_pinky_1',
@@ -14,21 +7,29 @@ MANO_RIGHT_KEYPOINTS = [
     'right_thumb', 'right_index', 'right_middle', 'right_ring', 'right_pinky'
 ]
 
+MANO_LEFT_KEYPOINTS = [
+    x.replace('right_', 'left_') for x in MANO_RIGHT_KEYPOINTS
+]
+
+# Re-arranged order is compatible with the output of manolayer
+# from official [manopth](https://github.com/hassony2/manopth)
+MANO_REORDER_MAP = [
+    0, 13, 14, 15, 16, 1, 2, 3, 17, 4, 5, 6, 18, 10, 11, 12, 19, 7, 8, 9, 20
+]
+
+MANO_RIGHT_REORDER_KEYPOINTS = [
+    MANO_RIGHT_KEYPOINTS[i] for i in MANO_REORDER_MAP
+]
+MANO_LEFT_REORDER_KEYPOINTS = [
+    MANO_LEFT_KEYPOINTS[i] for i in MANO_REORDER_MAP
+]
+
 MANO_LEFT_REORDER_KEYPOINTS = [
     'left_wrist', 'left_thumb_1', 'left_thumb_2', 'left_thumb_3', 'left_thumb',
     'left_index_1', 'left_index_2', 'left_index_3', 'left_index',
     'left_middle_1', 'left_middle_2', 'left_middle_3', 'left_middle',
     'left_ring_1', 'left_ring_2', 'left_ring_3', 'left_ring', 'left_pinky_1',
     'left_pinky_2', 'left_pinky_3', 'left_pinky'
-]
-
-MANO_RIGHT_REORDER_KEYPOINTS = [
-    'right_wrist', 'right_thumb_1', 'right_thumb_2', 'right_thumb_3',
-    'right_thumb', 'right_index_1', 'right_index_2', 'right_index_3',
-    'right_index', 'right_middle_1', 'right_middle_2', 'right_middle_3',
-    'right_middle', 'right_ring_1', 'right_ring_2', 'right_ring_3',
-    'right_ring', 'right_pinky_1', 'right_pinky_2', 'right_pinky_3',
-    'right_pinky'
 ]
 
 # Deprecated: reserved for backward compatibility
