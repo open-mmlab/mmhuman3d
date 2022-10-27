a9ab33dc6aea1d7cc99d88da1abf73932f6d7211
KHao123 <2427870449@qq.com>
2022-10-27 13:49:09 +0800

    fix mediapipe

--- mmhuman3d/core/conventions/keypoints_mapping/mediapip.py
+++ mmhuman3d/core/conventions/keypoints_mapping/mediapip.py
@@ -1,10 +1,10 @@
 BODY_KEYPOINTS = [
     'nose',
     'left_eye_4',
-    'left_eyeball',
+    'left_eye',
     'left_eye_1',
     'right_eye_4',
-    'right_eyeball',
+    'right_eye',
     'right_eye_1',
     'left_ear',
     'right_ear',
@@ -16,12 +16,12 @@ BODY_KEYPOINTS = [
     'right_elbow',
     'left_wrist',
     'right_wrist',
-    'left_pinky',
-    'right_pinky',
-    'left_index',
-    'right_index',
-    'left_thumb',
-    'right_thumb',
+    'left_pinky_1',
+    'right_pinky_1',
+    'left_index_1',
+    'right_index_1',
+    'left_thumb_1',
+    'right_thumb_1',
     'left_hip',
     'right_hip',
     'left_knee',
@@ -52,7 +52,7 @@ MANO_LEFT_KEYPOINTS = [
 # from official [manopth](https://github.com/hassony2/manopth)
 # remove wrist
 MANO_REORDER_MAP = [
-    13, 14, 15, 16, 1, 2, 3, 17, 4, 5, 6, 18, 10, 11, 12, 19, 7, 8, 9, 20
+    0, 13, 14, 15, 16, 1, 2, 3, 17, 4, 5, 6, 18, 10, 11, 12, 19, 7, 8, 9, 20
 ]
 
 MANO_RIGHT_REORDER_KEYPOINTS = [
@@ -62,12 +62,35 @@ MANO_LEFT_REORDER_KEYPOINTS = [
     MANO_LEFT_KEYPOINTS[i] for i in MANO_REORDER_MAP
 ]
 
-# Deprecated: reserved for backward compatibility
-MANO_KEYPOINTS = MANO_RIGHT_KEYPOINTS
-# Two hands (left + right)
-MANO_HANDS_KEYPOINTS = MANO_LEFT_KEYPOINTS + MANO_RIGHT_KEYPOINTS
-# Reordered two hands (left + right)
 MANO_HANDS_REORDER_KEYPOINTS = \
         MANO_LEFT_REORDER_KEYPOINTS + MANO_RIGHT_REORDER_KEYPOINTS
 
-WHOLE_BODY_KEYPOINTS = BODY_KEYPOINTS + MANO_HANDS_REORDER_KEYPOINTS
+WHOLE_BODY_KEYPOINTS = [
+    'nose',
+    'left_eye_4',
+    'left_eye',
+    'left_eye_1',
+    'right_eye_4',
+    'right_eye',
+    'right_eye_1',
+    'left_ear',
+    'right_ear',
+    'left_mouth_1',
+    'right_mouth_1',
+    'left_shoulder',
+    'right_shoulder',
+    'left_elbow',
+    'right_elbow',
+    'left_hip',
+    'right_hip',
+    'left_knee',
+    'right_knee',#
+    'left_ankle',#
+    'right_ankle',#
+    'left_heel',#
+    'right_heel',#
+    'left_foot',#
+    'right_foot', #
+]
+
+WHOLE_BODY_KEYPOINTS = WHOLE_BODY_KEYPOINTS + MANO_HANDS_REORDER_KEYPOINTS
