f26d39908fe8f3d13ed4a5bef49891a557ec5376
LAPTOP-V2NPSHUB\19656 <1965637476@qq.com>
2023-02-15 08:49:07 +0100

    add test for cliff head

---
+++ tests/test_models/test_heads/test_cliff_head.py
@@ -0,0 +1,45 @@
+import numpy as np
+import torch
+
+from mmhuman3d.models.heads.builder import CliffHead
+
+def test_cliff_head():
+    # initialize models
+    model = CliffHead(
+        feat_dim=2048,
+        img_res=(192, 256),
+        smpl_mean_params='data/body_models/smpl_mean_params.npz')
+
+    # image feature from backbone
+    batch_size = 32
+    x0_shape = (batch_size, 2048, 7, 7)
+    x0 = _demo_head_inputs(x0_shape)
+    x0 = torch.tensor(x0).float()
+    y0 = model(x0)
+    assert y0['pred_pose'].shape == (batch_size, 24, 3, 3)
+    assert y0['pred_shape'].shape == (batch_size, 10)
+    assert y0['pred_cam'].shape == (batch_size, 3)
+
+    # image feature from multi-layer backbone
+    x1_1_shape = (batch_size, 1024, 14, 14)
+    x1_2_shape = (batch_size, 2048, 7, 7)
+    x1 = [_demo_head_inputs(x1_1_shape), _demo_head_inputs(x1_2_shape)]
+    y1 = model(x1)
+    assert y1['pred_pose'].shape == (batch_size, 24, 3, 3)
+    assert y1['pred_shape'].shape == (batch_size, 10)
+    assert y1['pred_cam'].shape == (batch_size, 3)
+
+
+
+def _demo_head_inputs(input_shape=(1, 3, 64, 64)):
+    """Create a superset of inputs needed to run models.
+
+    Args:
+        input_shape (tuple): input batch dimensions.
+            Default: (1, 3, 64, 64).
+    """
+    features = np.random.random(input_shape)
+    features = torch.FloatTensor(features)
+
+    return features
+
