a87be63b8c6dd9211c528daca83adf141f5782d4
Wei-Chen-hub <1259566226@qq.com>
2023-04-25 16:58:56 +0800

    add init for breakdance

---
+++ mmhuman3d/data/data_converters/bracedance.py
@@ -0,0 +1,55 @@
+import os
+from typing import List
+
+import time
+import numpy as np
+import pandas as pd
+import json
+import cv2
+import glob
+import random
+from tqdm import tqdm
+import torch
+import smplx
+import ast
+
+from mmhuman3d.core.cameras import build_cameras
+from mmhuman3d.core.conventions.keypoints_mapping import convert_kps
+from mmhuman3d.data.data_structures.human_data import HumanData
+from .base_converter import BaseModeConverter
+from .builder import DATA_CONVERTERS
+# import mmcv
+from mmhuman3d.models.body_models.builder import build_body_model
+# from mmhuman3d.core.conventions.keypoints_mapping import smplx
+from mmhuman3d.core.conventions.keypoints_mapping import get_keypoint_idx, get_keypoint_idxs_by_part
+from mmhuman3d.models.body_models.utils import transform_to_camera_frame
+
+import pdb
+
+
+
+@DATA_CONVERTERS.register_module()
+class BraceConverter(BaseModeConverter):
+
+    ACCEPTED_MODES = ['train']
+
+    def __init__(self, modes: List = []) -> None:
+
+        super(BraceConverter, self).__init__(modes)
+
+    def convert_by_mode(self, dataset_path: str, out_path: str,
+                        mode: str) -> dict:
+        """
+        Args:
+            dataset_path (str): Path to directory where raw images and
+            annotations are stored.
+            out_path (str): Path to directory to save preprocessed npz file
+            mode (str): Mode in accepted modes
+
+        Returns:
+            dict:
+                A dict containing keys image_path, bbox_xywh, keypoints2d,
+                keypoints2d_mask, keypoints3d, keypoints3d_mask, cam_param
+                stored in HumanData() format
+        """
+        pass
