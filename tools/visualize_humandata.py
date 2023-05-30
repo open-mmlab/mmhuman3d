521cc043ecf993dd996f55056564ac1d6848ebc7
Wei-Chen-hub <1259566226@qq.com>
2023-05-30 21:14:48 +0800

    update truncation 0530

--- tools/visualize_humandata.py
+++ tools/visualize_humandata.py
@@ -9,6 +9,8 @@ import pyrender
 import trimesh
 import PIL.Image as pil_img
 
+from mmhuman3d.core.conventions.keypoints_mapping import get_keypoint_idx, get_keypoint_idxs_by_part
+
 from mmhuman3d.models.body_models.builder import build_body_model
 
 import pdb
@@ -59,17 +61,147 @@ def render_pose(img, body_model_param, body_model, camera):
     color = color.astype(np.float32) / 255.0
     # alpha = 1.0  # set transparency in [0.0, 1.0]
     # color[:, :, -1] = color[:, :, -1] * alpha
-
     valid_mask = (color[:, :, -1] > 0)[:, :, np.newaxis]
     img = img / 255
     # output_img = (color[:, :, :-1] * valid_mask + (1 - valid_mask) * img)
     output_img = (color[:, :, :] * valid_mask + (1 - valid_mask) * img)
+
+    # output_img = color
 
     img = (output_img * 255).astype(np.uint8)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     return img
 
+
+def render_truncation(img, body_model_param, body_model, cam_s, cam_b, cam_scale):
+
+    # the inverse is same
+    camera = cam_s
+    pyrender2opencv = np.array([[1.0, 0, 0, 0],
+                                [0, -1, 0, 0],
+                                [0, 0, -1, 0],
+                                [0, 0, 0, 1]])
+    
+    output = body_model(**body_model_param, return_verts=True)
+    
+    vertices = output['vertices'].detach().cpu().numpy().squeeze()
+    faces = body_model.faces
+
+    keypoints_3d = output['joints'].detach().cpu().numpy()
+    pelvis_pyrender = keypoints_3d[0, 0]
+
+    # render material
+    base_color = (1.0, 193/255, 193/255, 1.0)
+    material = pyrender.MetallicRoughnessMaterial(
+            metallicFactor=0.0,
+            alphaMode='OPAQUE',
+            baseColorFactor=base_color)
+
+    # get body mesh
+    body_trimesh = trimesh.Trimesh(vertices, faces, process=False)
+
+    body_mesh_s = pyrender.Mesh.from_trimesh(body_trimesh, material=material)
+    body_mesh_b = pyrender.Mesh.from_trimesh(body_trimesh, material=material)
+
+    # project body centroid to image
+    body_centroid = body_mesh_s.centroid
+    # cam_s_intrinsics = cam_s.get_projection_matrix(img.shape[1], img.shape[0])
+    cam_s_intrinsics = np.array([[cam_s.fx, 0, cam_s.cx],
+                                 [0, cam_s.fy, cam_s.cy],
+                                 [0, 0, 1]])
+    body_centroid_s = cam_s_intrinsics @ body_centroid
+    body_centroid_s = (body_centroid_s / body_centroid_s[-1])[:2]
+
+    cam_b_intrinsics = np.array([[cam_b.fx, 0, cam_b.cx],
+                                [0, cam_b.fy, cam_b.cy],
+                                [0, 0, 1]])
+    body_centroid_b = cam_b_intrinsics @ body_centroid
+    body_centroid_b = (body_centroid_b / body_centroid_b[-1])[:2]
+
+    # prepare camera and light
+    light_s = pyrender.DirectionalLight(color=np.ones(3), intensity=2.0)
+    light_b = pyrender.DirectionalLight(color=np.ones(3), intensity=2.0)
+    cam_pose = pyrender2opencv @ np.eye(4)
+    
+    # build scene_s
+    scene_s = pyrender.Scene(bg_color=[0.0, 0.0, 0.0, 0.0],
+                                    ambient_light=(0.3, 0.3, 0.3))
+    scene_s.add(cam_s, pose=cam_pose)
+    scene_s.add(light_s, pose=cam_pose)
+    scene_s.add(body_mesh_s, 'mesh')
+
+    # render scene_s and get mask
+    rs = pyrender.OffscreenRenderer(viewport_width=img.shape[1] * cam_scale,
+                                    viewport_height=img.shape[0] * cam_scale,
+                                    point_size=1.0)
+    color_s, _ = rs.render(scene_s, flags=pyrender.RenderFlags.RGBA)
+    color_s = color_s.astype(np.float32) / 255.0
+    valid_mask_s = (color_s[:, :, -1] > 0)[:, :, np.newaxis]
+
+    img_s = (color_s * 255).astype(np.uint8)
+    img_s = cv2.cvtColor(img_s, cv2.COLOR_BGR2RGB)
+
+    # build scene_b
+    scene_b = pyrender.Scene(bg_color=[0.0, 0.0, 0.0, 0.0],
+                                    ambient_light=(0.3, 0.3, 0.3))
+    scene_b.add(cam_b, pose=cam_pose)
+    scene_b.add(light_b, pose=cam_pose)
+    scene_b.add(body_mesh_b, 'mesh')
+
+    # render scene_b and get mask
+    rb = pyrender.OffscreenRenderer(viewport_width=img.shape[1] * cam_scale,
+                                    viewport_height=img.shape[0] * cam_scale,
+                                    point_size=1.0)
+    color_b, _ = rb.render(scene_b, flags=pyrender.RenderFlags.RGBA)
+    color_b = color_b.astype(np.float32) / 255.0
+    valid_mask_b = (color_b[:, :, -1] > 0)[:, :, np.newaxis]
+
+    img_b = (color_b * 255).astype(np.uint8)
+    img_b = cv2.cvtColor(img_b, cv2.COLOR_BGR2RGB)
+
+    # # plot point on image
+    # img_s = cv2.circle(img_s, (int(body_centroid_s[0]), int(body_centroid_s[1])), 10, (0, 0, 255), -1)
+    # img_b = cv2.circle(img_b, (int(body_centroid_b[0]), int(body_centroid_b[1])), 10, (0, 0, 255), -1)
+
+    # resize img_s to larger resolution
+    img_s = cv2.resize(img_s, None, fx=cam_scale**2, fy=cam_scale**2, interpolation=cv2.INTER_NEAREST)
+    valid_mask_s = (img_s[:, :, -1] > 0)[:, :, np.newaxis]
+    # adjust body centroid accordingly
+    body_centroid_s = (body_centroid_s * cam_scale**2).astype(np.int32)
+
+    # align left top corner of img_b to img_s
+    body_centroid_b = body_centroid_b.astype(np.int32)
+    img_b_lefttop = body_centroid_s - body_centroid_b
+
+    # pad image_b for overlay, note that ima_b and img_b_lefttop are in different (W, H) order
+    img_b = cv2.cvtColor(img_b, cv2.COLOR_RGB2BGR)
+    img_s = np.pad(img_b, ((img_b_lefttop[1], img_s.shape[0] - img_b.shape[0] - img_b_lefttop[1]), 
+                           (img_b_lefttop[0], img_s.shape[1] - img_b.shape[1] - img_b_lefttop[0]), (0, 0)), 
+                           mode='constant')+ img_s
+
+    # calculate truncation
+    in_screen_mask_s = valid_mask_s[img_b_lefttop[1]:img_b_lefttop[1] + img_b.shape[0] +1,
+                                    img_b_lefttop[0]:img_b_lefttop[0] + img_b.shape[1] +1,
+                                    :]
+    # in_screen_mask_s = valid_mask_s[:img_b.shape[0],:img_b.shape[1],:]
+    # in_screen_mask_s = np.pad(in_screen_mask_s, ((0, padding_width), (0, padding_height), (0, 0)), mode='constant')
+    trunc = 1 - np.sum(in_screen_mask_s) / np.sum(valid_mask_s)
+
+    # crop image_s to its bbox
+    rows, cols, _ = np.where(valid_mask_s)
+    min_row, max_row = np.min(rows), np.max(rows)
+    min_col, max_col = np.min(cols), np.max(cols)
+    cropped_image = img_s[min_row:max_row+1, min_col:max_col+1, :]
+
+    return cropped_image, trunc, img_s, img_b
+
+
+def calculate_pp_occlusion(masks, h, w):
+
+    # sum all masks
+    return
+    mask_all = a
 
 
 
@@ -83,7 +215,7 @@ def visualize_humandata(args):
         'gta_human2': '/mnt/e/gta_human2_multiple',
         'renbody': '/mnt/d/renbody',
         'egobody': '/mnt/d/egobody',
-        'ubody': '/mnt/e/ubody',
+        'ubody': '/mnt/d/ubody',
         'ssp3d': '/mnt/e/ssp-3d',
         'FIT3D': '/mnt/d/sminchisescu-research-datasets',
         'CHI3D': '/mnt/d/sminchisescu-research-datasets',
@@ -146,7 +278,7 @@ def visualize_humandata(args):
                     gender=gender,
                     num_betas=10,
                     use_face_contour=True,
-                    flat_hand_mean=True,
+                    flat_hand_mean=False,
                     use_pca=False,
                     batch_size=1
                 )).to(device)
@@ -168,7 +300,7 @@ def visualize_humandata(args):
                     batch_size=1
                 )).to(device)
     
-    idx_list = [100, 500, 1000, 500, 10000, 15000]
+    # idx_list = [57072]
 
     # for idx in idx_list:
     for i in range(10):
@@ -200,8 +332,16 @@ def visualize_humandata(args):
         camera = pyrender.camera.IntrinsicsCamera(
             fx=focal_length[0], fy=focal_length[1],
             cx=camera_center[0], cy=camera_center[1])
-
-
+        
+        # prepare camera for calculating truncation and pp occlusion
+        cam_scale = 2
+        camera_small = pyrender.camera.IntrinsicsCamera(
+            fx=focal_length[0] / cam_scale, fy=focal_length[1] / cam_scale,
+            cx=camera_center[0] * cam_scale, cy=camera_center[1] * cam_scale)
+        camera_big = pyrender.camera.IntrinsicsCamera(
+            fx=focal_length[0] * cam_scale, fy=focal_length[1] * cam_scale,
+            cx=camera_center[0] * cam_scale, cy=camera_center[1] * cam_scale)
+ 
         # read gender
         if has_gender:
             gender = param['meta'].item()['gender'][idx]
@@ -210,20 +350,45 @@ def visualize_humandata(args):
 
         # read smpl smplx params and build body model
         if has_smpl:
-            body_model_param_tensor = {key: torch.tensor(body_model_param_smpl[key][idx:idx+1], device=device)
-                            for key in body_model_param_smpl.keys()}
+            body_model_param_tensor = {key: torch.tensor(body_model_param_smpl[key][idx:idx+1],
+                                                          device=device, dtype=torch.float32)
+                            for key in body_model_param_smpl.keys()
+                            if body_model_param_smpl[key][idx:idx+1].shape[0] > 0}
             rendered_image = render_pose(img=image, 
                                         body_model_param=body_model_param_tensor, 
                                         body_model=smpl_model[gender],
                                         camera=camera)
 
         if has_smplx:
-            body_model_param_tensor = {key: torch.tensor(body_model_param_smplx[key][idx:idx+1], device=device)
-                            for key in body_model_param_smplx.keys()}
+            body_model_param_tensor = {key: torch.tensor(body_model_param_smplx[key][idx:idx+1],
+                                                          device=device, dtype=torch.float32)
+                            for key in body_model_param_smplx.keys()
+                            if body_model_param_smplx[key][idx:idx+1].shape[0] > 0}
             rendered_image = render_pose(img=image, 
                                         body_model_param=body_model_param_tensor, 
                                         body_model=smplx_model[gender],
                                         camera=camera)
+            trunc_image, trunc, img_s, img_b = render_truncation(img=image,
+                                        body_model_param=body_model_param_tensor, 
+                                        body_model=smplx_model[gender],
+                                        cam_s=camera_small,
+                                        cam_b=camera_big,
+                                        cam_scale=cam_scale)
+            
+
+
+
+        # trunc = calculate_truncation(mask, image.shape[0], image.shape[1])
+        # trunc = 1
+
+        # write truncation on image
+        cv2.putText(rendered_image, f'Truncation: {trunc}', 
+                    (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4, cv2.LINE_AA)
+
+        print(f'Truncation: {trunc}')
+
+        
+
 
         os.makedirs(os.path.join(args.out_path, args.dataset_name), exist_ok=True)
         out_image_path = os.path.join(args.out_path, args.dataset_name,
@@ -231,74 +396,56 @@ def visualize_humandata(args):
         print(f'Saving image to {out_image_path}')
         cv2.imwrite(out_image_path, rendered_image)
 
-        # pdb.set_trace()
+        if args.save_trunc:
+            out_image_path_trunc = os.path.join(args.out_path, args.dataset_name,
+                                f'{os.path.basename(param_ps[0])[:-4]}_{idx}_trunc.png')
+            cv2.imwrite(out_image_path_trunc, trunc_image)
+
+            out_image_path_s = os.path.join(args.out_path, args.dataset_name,
+                                f'{os.path.basename(param_ps[0])[:-4]}_{idx}_s.png')
+            cv2.imwrite(out_image_path_s, img_s)
+
+            out_image_path_b = os.path.join(args.out_path, args.dataset_name,
+                                f'{os.path.basename(param_ps[0])[:-4]}_{idx}_b.png')
+            cv2.imwrite(out_image_path_b, img_b)
 
 
+
+
+
+        pdb.set_trace()
 
         continue
 
 
 
-        vertices = output['vertices'].detach().cpu().numpy().squeeze()
-        joints = output['joints'].detach().cpu().numpy().squeeze()  # in openpose topology
-        body = trimesh.Trimesh(vertices, process=False)
-
-
-
-        body_mesh = pyrender.Mesh.from_trimesh(body, material=material)
-
-        pyrender2opencv = np.array([[1.0, 0, 0, 0],
-                                    [0, -1, 0, 0],
-                                    [0, 0, -1, 0],
-                                    [0, 0, 0, 1]])
-        # image.shape = [9999, 9999]
-        
-        # build camera_pose and light
-        camera_pose = np.eye(4)
-        camera_pose = np.array([1.0, 1.0, 1.0, 1.0]).reshape(-1, 1) * camera_pose
-        light = pyrender.DirectionalLight(color=np.ones(3), intensity=4.0)
-
-        # build scene
-        scene = pyrender.Scene(bg_color=[0.0, 0.0, 0.0, 0.0],
-                                       ambient_light=(0.3, 0.3, 0.3))
-        scene.add(camera, pose=pyrender2opencv)
-        scene.add(light, pose=pyrender2opencv)
-        scene.add(body_mesh, 'mesh')
-
-        # render scene
-        os.environ["PYOPENGL_PLATFORM"] = "osmesa" # include this line if use in vscode
-        r = pyrender.OffscreenRenderer(viewport_width=image.shape[1],
-                                        viewport_height=image.shape[0],
-                                        point_size=1.0)
-        color, _ = r.render(scene, flags=pyrender.RenderFlags.RGBA)
-        color = color.astype(np.float32) / 255.0
-        alpha = 1.0  # set transparency in [0.0, 1.0]
-        color[:, :, -1] = color[:, :, -1] * alpha
-        pdb.set_trace()
-        color = pil_img.fromarray((color * 255).astype(np.uint8))
-
-        # concat on image
-        output_img = pil_img.fromarray((image).astype(np.uint8))
-        pdb.set_trace()
-        output_img.paste(color, (0, 0, image.shape[1], image.shape[0]), color)
-        # output_img.paste(color)
-
-        # save image (downsize)
-        output_img.convert('RGB')
-        # output_img = output_img.resize((int(image.shape[1] / args.scale), int(image.shape[0] / args.scale)))
-        output_img.save(os.path.join(args.out_path, 
-                        f'{os.path.basename(param_ps[0])[:-4]}_{idx}.png'))
-
-        pdb.set_trace()
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_name', type=str, required=False, help='which dataset', default='renbody')
+    parser.add_argument('--dataset_name', type=str, required=False, 
+                        help='which dataset', 
+                        default='ubody')
     # parser.add_argument('--dataset_path', type=str, required=True, help='path to the dataset')
-    parser.add_argument('--out_path', type=str, required=False, help='path to the output folder',
+    parser.add_argument('--out_path', type=str, required=False, 
+                        help='path to the output folder',
                         default='/mnt/c/users/12595/desktop/humandata_vis')
+    
+
+    # optional args
+    parser.add_argument('--save_trunc', type=bool, required=False, 
+                        help='save truncation images', 
+                        default=True)
+    parser.add_argument('--save_pp_occlusion', type=bool, required=False,
+                        help='save pp occlusion images',
+                        default=False)
+
+
+
+
+
+
 
     args = parser.parse_args()
     visualize_humandata(args)
